1112bcb2bf6c1e86e511fe454b4beccc0b2937ec
dj <dj>
1995-03-21 10:11:16 +0000

    import djgpp 2.00

---
+++ src/debug/common/symify.c
@@ -0,0 +1,135 @@
+/* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pc.h>
+#include <debug/syms.h>
+#include <debug/tss.h>
+
+#define SC(r,c) (*(char *)(sc + (r)*ScreenCols() + (c)))
+#define SW(r,c) (*(sc + (r)*ScreenCols() + (c)))
+
+TSS a_tss;
+int main(int argc, char **argv)
+{
+  int r, c;
+  short *sc;
+  char buf[90];
+  int i, lineno;
+  unsigned v;
+  unsigned long d;
+  char *func, *file;
+  FILE *ofile=0;
+  FILE *ifile=0;
+
+  if (argc < 2)
+  {
+    fprintf(stderr, "Usage: symify [-o <outfile>] [-i <corefile>] <program>\n");
+    fprintf(stderr, "This program adds debug information to DJGPP program call frame tracebacks\n");
+    return 1;
+  }
+  while (argv[1][0] == '-')
+  {
+    if ((strcmp(argv[1], "-o") == 0) && (argc > 3))
+    {
+      ofile = fopen(argv[2], "w");
+      if (ofile == 0)
+        fprintf(stderr, "Error: unable to open file %s\n", argv[2]);
+      argc -= 2;
+      argv += 2;
+    }
+    if ((strcmp(argv[1], "-i") == 0) && (argc > 3))
+    {
+      ifile = fopen(argv[2], "r");
+      if (ifile == 0)
+        fprintf(stderr, "Error: unable to open file %s\n", argv[2]);
+      argc -= 2;
+      argv += 2;
+    }
+  }
+  syms_init(argv[1]);
+
+  if (ifile)
+  {
+    char line[1000];
+    if (ofile == 0)
+      ofile = stdout;
+    while (fgets(line, 1000, ifile))
+    {
+      if (strncmp(line, "  0x", 4) == 0)
+      {
+        sscanf(line+4, "%x", &v);
+        func = syms_val2name(v, &d);
+        file = syms_val2line(v, &lineno, 0);
+        fprintf(ofile, "  0x%08x", v);
+        if (func)
+        {
+          fprintf(ofile, " %s", func);
+          if (d)
+            fprintf(ofile, "%+ld", d);
+        }
+        if (file)
+        {
+          if (func)
+            fprintf(ofile, ", ");
+          fprintf(ofile, "line %d of %s", lineno, file);
+        }
+        fputc('\n', ofile);
+      }
+      else
+        fputs(line, ofile);
+    }
+    return 0;
+  }
+
+  sc = (short *)malloc(ScreenRows() * ScreenCols() * 2);
+
+  ScreenRetrieve(sc);
+
+  for (r=0; r<ScreenRows(); r++)
+  {
+    if (SC(r,0) == ' ' && SC(r,1) == ' ' && SC(r,2) == '0' && SC(r,3) == 'x')
+    {
+      buf[8] = 0;
+      for (i=0; i<8; i++)
+        buf[i] = SC(r, i+4);
+      sscanf(buf, "%x", &v);
+      func = syms_val2name(v, &d);
+      file = syms_val2line(v, &lineno, 0);
+      buf[0] = 0;
+      if (func)
+      {
+	strcpy(buf, func);
+	if (d)
+	  sprintf(buf+strlen(buf), "%+ld", d);
+      }
+      if (file)
+      {
+        if (buf[0])
+          strcat(buf, ", ");
+        sprintf(buf+strlen(buf), "line %d of %s", lineno, file);
+      }
+      if (buf[0])
+        for (i=0; buf[i]; i++)
+          SW(r, 15+i) = 0x0f00 + buf[i];
+    }
+  }
+
+  if (ofile)
+  {
+    for (r=0; r<ScreenRows(); r++)
+    {
+      c = 0;
+      for (i=0; i<ScreenCols(); i++)
+        if (SC(r, i) != ' ')
+          c = i;
+      for (i=0; i<=c; i++)
+        fputc(SC(r,i), ofile);
+      fputc('\n', ofile);
+    }
+    fclose(ofile);
+  }
+  else
+    ScreenUpdate(sc);
+  return 0;
+}
---
+++ src/debug/common/wild.c
@@ -0,0 +1,73 @@
+/* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
+/* This is file WILD.C */
+/*
+** Copyright (C) 1993 DJ Delorie, 24 Kirsten Ave, Rochester NH 03867-2954
+**
+** This file is distributed under the terms listed in the document
+** "copying.dj", available from DJ Delorie at the address above.
+** A copy of "copying.dj" should accompany this file; if not, a copy
+** should be available from where this file was obtained.  This file
+** may not be distributed without a verbatim copy of "copying.dj".
+**
+** This file is distributed WITHOUT ANY WARRANTY; without even the implied
+** warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#include <string.h>
+#include <debug/wild.h>
+
+int wild(char *pattern, char *string)
+{
+  int nlit;
+  while (*pattern)
+  {
+    switch (*pattern)
+    {
+      case '*':
+        pattern++;
+        if (*pattern == 0)
+          return 1;
+        nlit=0;
+        while ((pattern[nlit] != 0)
+            && (pattern[nlit] != '*')
+            && (pattern[nlit] != '?') )
+          nlit++;
+        while (1)
+        {
+          if (strncmp(string, pattern, nlit) == 0)
+            break;
+          string++;
+          if (*string == 0)
+            return 0;
+        }
+        break;
+      case '?':
+        if (*string == 0)
+          return 0;
+        pattern++;
+        string++;
+        break;
+      default:
+        if (*pattern != *string)
+          return 0;
+        pattern++;
+        string++;
+        break;
+    }
+  }
+  if (*string)
+    return 0;
+  return 1;
+}
+
+#ifdef DEBUG
+main(int argc, char **argv)
+{
+  int i;
+  if (argc < 3)
+    return 1;
+  for (i=2; argv[i]; i++)
+    printf("%s %s %d\n", argv[1], argv[i], wild(argv[1], argv[i]));
+  return 0;
+}
+#endif
