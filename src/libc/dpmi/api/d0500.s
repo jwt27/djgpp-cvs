19f1391bc54b39d0f0ae1cafe8847e9a1f80ee8f
dj <dj>
1995-03-21 10:44:14 +0000

    import djgpp 2.00

---
+++ src/libc/dpmi/api/d0500.s
@@ -0,0 +1,14 @@
+/* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
+#define USE_EDI
+#include "dpmidefs.h"
+
+	.text
+
+	FUNC(___dpmi_get_free_memory_information)
+	ENTER
+
+	movl	ARG1,%edi
+	DPMI(0x0500)
+	xorl	%eax,%eax
+
+	LEAVE
---
+++ src/libc/pc_hw/io/osl.s
@@ -0,0 +1,16 @@
+/* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
+#define USE_ESI
+
+#include <libc/asmdefs.h>
+
+	FUNC(_outportsl)	/* port buffer length */
+	ENTER
+
+	movl	ARG1,%edx
+	movl	ARG2,%esi
+	movl	ARG3,%ecx
+	cld
+	rep
+	outsl
+
+	LEAVE
