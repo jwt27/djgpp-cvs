83ef7066e2707a5b9b022d3553e0708c6a748672
dj <dj>
1995-03-27 12:12:32 +0000

    import djgpp 2.00

---
+++ tests/libc/crt0/globtest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <fnmatch.h>
+#include <glob.h>
+
+int
+main(int argc, char **argv)
+{
+  int i;
+  glob_t g;
+  printf("Pattern: `%s'\n", argv[1]);
+  glob(argv[1], 0, 0, &g);
+  for (i=0; i<g.gl_pathc; i++)
+    printf("-> `%s'\n", g.gl_pathv[i]);
+  return 0;
+}
