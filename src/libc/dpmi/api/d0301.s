7b8acce86c284fc9b9cb5f5c89c402cb1baa18d5
dj <dj>
1995-04-13 09:03:14 +0000

    import djgpp 2.00

---
+++ src/libc/dpmi/api/d0301.s
@@ -0,0 +1,21 @@
+/* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
+#define USE_EBX
+#define USE_EDI
+#include "dpmidefs.h"
+
+	.text
+
+	FUNC(___dpmi_simulate_real_mode_procedure_retf)
+	ENTER
+
+	pushl	%es
+	movw	___djgpp_ds_alias, %ax
+	movw	%ax, %es
+	
+	movl	$0, %ebx
+	movl	$0, %ecx
+	movl	ARG1, %edi
+	DPMI(0x0301)
+	xorl	%eax, %eax
+
+	LEAVEP(popl %es)
