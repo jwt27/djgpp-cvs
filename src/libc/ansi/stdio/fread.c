709b41c6a4037125a322d91490b847c64e659606
dj <dj>
1996-02-22 15:54:54 +0000

    import djgpp 2.01

--- src/libc/ansi/stdio/fread.c
+++ src/libc/ansi/stdio/fread.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 1996 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,28 +12,63 @@ fread(void *vptr, size_t size, size_t count, FILE *iop)
   int c;
 
   s = size * count;
-  while (s > 0) {
-    if (iop->_cnt < s) {
-      if (iop->_cnt > 0) {
-	memcpy(ptr, iop->_ptr, iop->_cnt);
-	ptr += iop->_cnt;
-	s -= iop->_cnt;
+  if(!__is_text_file(iop))
+  {
+    while (s > 0) {
+      if (iop->_cnt < s) {
+	if (iop->_cnt > 0) {
+	  memcpy(ptr, iop->_ptr, iop->_cnt);
+	  ptr += iop->_cnt;
+	  s -= iop->_cnt;
+	}
+	/*
+	 * filbuf clobbers _cnt & _ptr,
+	 * so don't waste time setting them.
+	 */
+	if ((c = _filbuf(iop)) == EOF)
+	  break;
+	*ptr++ = c;
+	s--;
       }
-      /*
-       * filbuf clobbers _cnt & _ptr,
-       * so don't waste time setting them.
-       */
-      if ((c = _filbuf(iop)) == EOF)
-	break;
-      *ptr++ = c;
-      s--;
+      if (iop->_cnt >= s) {
+	memcpy(ptr, iop->_ptr, s);
+	iop->_ptr += s;
+	iop->_cnt -= s;
+	return count;
+      }
     }
-    if (iop->_cnt >= s) {
-      memcpy(ptr, iop->_ptr, s);
-      iop->_ptr += s;
-      iop->_cnt -= s;
-      return count;
-    }
+  }
+  else
+  {
+    while (s > 0) {
+      if (iop->_cnt < s) {
+	while (iop->_cnt > 0) {
+	  if ((c = *iop->_ptr++) != '\r')
+	  {
+	    *ptr++ = c;
+	    s--;
+	  }
+	  iop->_cnt--;
+	}
+	if ((c = _filbuf(iop)) == EOF)
+	  break;
+	if (c != '\r')
+	{
+	  *ptr++ = c;
+	  s--;
+	}
+      }
+      if (iop->_cnt >= s) {
+	while (s > 0 && iop->_cnt > 0) {
+	  if ((c = *iop->_ptr++) != '\r')
+	  {
+	    *ptr++ = c;
+	    s--;
+	  }
+	  iop->_cnt--;
+	}
+      }
+    } /* end while */
   }
   return size != 0 ? count - ((s + size - 1) / size) : 0;
 }
