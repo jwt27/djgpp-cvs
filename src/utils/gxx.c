d950d365aaf391aa8b910a7736bf1a52d9aa9859
dj <dj>
1996-04-26 19:02:50 +0000

    import djgpp 2.01

--- src/utils/gxx.c
+++ src/utils/gxx.c
@@ -4,17 +4,19 @@
 #include <stdlib.h>
 
 char lgpp[] = "-lgpp";
+char lstdc[] = "-lstdcx";
 char lm[] = "-lm";
 
 int
 main(int argc, char **argv)
 {
-  char **newargs = (char **)malloc((argc+3) * sizeof(char *));
+  char **newargs = (char **)malloc((argc+4) * sizeof(char *));
   int i;
   for (i=0; i<argc; i++)
     newargs[i] = argv[i];
 
   newargs[i++] = lgpp;
+  newargs[i++] = lstdc;
   newargs[i++] = lm;
   newargs[i++] = 0;
 
