26f74f90d8eb6b68db64d777a94f31318203e695
dj <dj>
1996-04-27 14:24:12 +0000

    import djgpp 2.01

--- src/libc/ansi/stdio/stdprn.c
+++ src/libc/ansi/stdio/stdprn.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 1996 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #include <stdio.h>
 #include <libc/file.h>
@@ -5,6 +6,6 @@
 
 FILE __dj_stdprn = {
   0, 0, 0, 0,
-  _IOWRT | _IOLBF,
-  3
+  _IOWRT | _IONBF,
+  4
 };
