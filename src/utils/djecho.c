42b5b619294f22bf373e4e5c12b7243419c2c55c
dj <dj>
1996-08-12 23:55:54 +0000

    import djgpp 2.02

---
+++ src/utils/djecho.c
@@ -0,0 +1,64 @@
+/* Copyright (C) 1996 DJ Delorie, see COPYING.DJ for details */
+/* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+  int i;
+  int nflag=0;
+  FILE *out = stdout;
+  char sep = ' ';
+
+  while (argc > 1 && argv[1][0] == '-')
+  {
+    if ((argc > 2) && (strcmp(argv[1], "-o") == 0))
+    {
+      out = fopen(argv[2], "w");
+      if (!out)
+      {
+	perror(argv[2]);
+	out = stdout;
+      }
+      argc -= 2;
+      argv += 2;
+    }
+    else if ((argc > 2) && (strcmp(argv[1], "-a") == 0))
+    {
+      out = fopen(argv[2], "a");
+      if (!out)
+      {
+	perror(argv[2]);
+	out = stdout;
+      }
+      argc -= 2;
+      argv += 2;
+    }
+    else if ((argc > 1) && (strcmp(argv[1], "-n") == 0))
+    {
+      nflag = 1;
+      argc--;
+      argv++;
+    }
+    else if ((argc > 1) && (strcmp(argv[1], "-s") == 0))
+    {
+      sep = '\n';
+      argc--;
+      argv++;
+    }
+    else
+    {
+      fprintf(stderr, "Usage: echo [-o file] [-a file] [-n] [-s] args...\n");
+      return 1;
+    }
+  }
+  for (i=1; i<argc; i++)
+  {
+    if (i>1) fputc(sep, out);
+    fputs(argv[i], out);
+  }
+  if (!nflag)
+    fputc('\n', out);
+  fclose(out);
+  return 0;
+}
