dbc87581913d84417b99aa3a499fbe1c1dd58d7b
dj <dj>
1996-09-01 14:31:56 +0000

    import djgpp 2.01

--- include/sys/wait.h
+++ include/sys/wait.h
@@ -12,13 +12,13 @@ extern "C" {
 
 #include <sys/djtypes.h>
 
-#define WEXITSTATUS(stat_val)	(  (stat_val) & 0x00ff)
-#define WIFEXITED(stat_val)	(!((stat_val) & 0xff00))
-#define WIFSIGNALED(stat_val)	( ((stat_val) & 0xff00))
+#define WEXITSTATUS(stat_val)	(  (stat_val) & 0x000ff)
+#define WIFEXITED(stat_val)	(!((stat_val) & 0x3ff00))
+#define WIFSIGNALED(stat_val)	( ((stat_val) & 0x3ff00))
 #define WIFSTOPPED(stat_val)	0
 #define WNOHANG			1
 #define WSTOPSIG(stat_val)	0
-#define WTERMSIG(stat_val)	(  (stat_val) & 0x00ff)
+#define WTERMSIG(stat_val)	( ((stat_val) >> 8 ) & 0x3ff)
 #define WUNTRACED
 
 __DJ_pid_t
