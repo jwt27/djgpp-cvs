ce23528ffcaef6d33f0c3cbd4d2d2da5d0211ec7
dj <dj>
1996-09-08 21:05:22 +0000

    import djgpp 2.01

---
+++ src/libc/compat/termios/cfstspd.c
@@ -0,0 +1,20 @@
+#include <errno.h>
+#include <stddef.h>
+#include <termios.h>
+
+int
+cfsetspeed (struct termios *termiosp, speed_t speed)
+{
+  /* check arguments */
+  if (termiosp == NULL)
+    {
+      errno = EINVAL;
+      return -1;
+    }
+
+  /* set input and output speed */
+  termiosp->c_ispeed = speed;
+  termiosp->c_ospeed = speed;
+
+  return 0;
+}
