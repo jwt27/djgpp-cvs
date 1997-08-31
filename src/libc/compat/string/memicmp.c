8771d0f147badd83d229c98c3c7e7448f3a810a4
dj <dj>
1997-08-31 15:32:06 +0000

    import djgpp 2.02

---
+++ src/libc/compat/string/memicmp.c
@@ -0,0 +1,23 @@
+/* Copyright (C) 1997 DJ Delorie, see COPYING.DJ for details */
+#include <string.h>
+#include <ctype.h>
+
+int
+memicmp(const void *s1, const void *s2, size_t n)
+{
+  if (n != 0)
+  {
+    const unsigned char *p1 = s1, *p2 = s2;
+
+    do {
+      if (*p1 != *p2)
+      {
+        int c = toupper(*p1) - toupper(*p2);
+	if (c)
+	  return c;
+      }
+      p1++; p2++;
+    } while (--n != 0);
+  }
+  return 0;
+}
