960c8375d6f8c37f3e5dae1d629de6e45ea4eff8
dj <dj>
1997-08-31 16:07:06 +0000

    import djgpp 2.02

--- src/libc/pc_hw/kb/getkey.c
+++ src/libc/pc_hw/kb/getkey.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 1997 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #include <pc.h>
 #include <dpmi.h>
@@ -9,7 +10,7 @@ getkey(void)
   r.h.ah = 0x10;
   __dpmi_int(0x16, &r);
 
-  if (r.h.al == 0x00 || r.h.al == 0xe0)
+  if (r.h.al == 0x00 || (r.h.al == 0xe0 && r.h.ah != 0))
     return 0x0100 | r.h.ah;
   return r.h.al;
 }
--- src/libc/pc_hw/kb/getxkey.c
+++ src/libc/pc_hw/kb/getxkey.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 1997 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #include <pc.h>
 #include <dpmi.h>
@@ -11,7 +12,7 @@ getxkey(void)
 
   if (r.h.al == 0x00)
     return 0x0100 | r.h.ah;
-  if (r.h.al == 0xe0)
+  if (r.h.al == 0xe0 && r.h.ah != 0)
     return 0x0200 | r.h.ah;
   return r.h.al;
 }
