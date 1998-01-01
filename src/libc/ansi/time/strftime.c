e7a82db06e5241311dd487414c461266d1a98676
dj <dj>
1998-01-01 16:42:52 +0000

    import djgpp 2.02

--- src/libc/ansi/time/strftime.c
+++ src/libc/ansi/time/strftime.c
@@ -1,6 +1,8 @@
+/* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 #define TM_YEAR_BASE 1900
 
@@ -24,7 +26,7 @@ static size_t gsize;
 static char *pt;
 
 static int
-_add(const char *str)
+_add(const char *str, int upcase)
 {
   for (;; ++pt, --gsize)
   {
@@ -32,6 +34,8 @@ _add(const char *str)
       return 0;
     if (!(*pt = *str++))
       return 1;
+    if (upcase)
+      *pt = toupper(*pt);
   }
 }
 
@@ -39,21 +43,37 @@ static int
 _conv(int n, int digits, char pad)
 {
   static char buf[10];
-  char *p;
+  char *p = buf + sizeof(buf) - 2;
 
-  for (p = buf + sizeof(buf) - 2; n > 0 && p > buf; n /= 10, --digits)
+  do {
     *p-- = n % 10 + '0';
-  while (p > buf && digits-- > 0)
-    *p-- = pad;
-  return _add(++p);
+    n /= 10;
+    --digits;
+  } while (n > 0 && p > buf);
+
+  if (pad)
+    while (p > buf && digits-- > 0)
+      *p-- = pad;
+  return _add(++p, 0);
 }
 
 static size_t
-_fmt(const char *format, const struct tm *t)
+_fmt(const char *format, const struct tm *t, int upcase)
 {
   for (; *format; ++format)
   {
     if (*format == '%')
+    {
+      int pad = '0', space=' ';
+      if (format[1] == '_')
+	pad = space = ' ', format++;
+      if (format[1] == '-')
+	pad = space = 0, format++;
+      if (format[1] == '0')
+	pad = space = '0', format++;
+      if (format[1] == '^')
+	upcase = 1, format++;
+
       switch(*++format)
       {
       case '\0':
@@ -62,34 +82,34 @@ _fmt(const char *format, const struct tm *t)
       case 'A':
 	if (t->tm_wday < 0 || t->tm_wday > 6)
 	  return 0;
-	if (!_add(Afmt[t->tm_wday]))
+	if (!_add(Afmt[t->tm_wday], upcase))
 	  return 0;
 	continue;
       case 'a':
 	if (t->tm_wday < 0 || t->tm_wday > 6)
 	  return 0;
-	if (!_add(afmt[t->tm_wday]))
+	if (!_add(afmt[t->tm_wday], upcase))
 	  return 0;
 	continue;
       case 'B':
 	if (t->tm_mon < 0 || t->tm_mon > 11)
 	  return 0;
-	if (!_add(Bfmt[t->tm_mon]))
+	if (!_add(Bfmt[t->tm_mon], upcase))
 	  return 0;
 	continue;
       case 'b':
       case 'h':
 	if (t->tm_mon < 0 || t->tm_mon > 11)
 	  return 0;
-	if (!_add(bfmt[t->tm_mon]))
+	if (!_add(bfmt[t->tm_mon], upcase))
 	  return 0;
 	continue;
       case 'C':
-	if (!_fmt("%a %b %e %H:%M:%S %Y", t))
+	if (!_conv((t->tm_year+TM_YEAR_BASE)/100, 2, pad))
 	  return 0;
 	continue;
       case 'c':
-	if (!_fmt("%m/%d/%y %H:%M:%S", t))
+	if (!_fmt("%a %b %e %H:%M:%S %Y", t, upcase))
 	  return 0;
 	continue;
       case 'e':
@@ -97,24 +117,24 @@ _fmt(const char *format, const struct tm *t)
 	  return 0;
 	continue;
       case 'D':
-	if (!_fmt("%m/%d/%y", t))
+	if (!_fmt("%m/%d/%y", t, upcase))
 	  return 0;
 	continue;
       case 'd':
-	if (!_conv(t->tm_mday, 2, '0'))
+	if (!_conv(t->tm_mday, 2, pad))
 	  return 0;
 	continue;
       case 'H':
-	if (!_conv(t->tm_hour, 2, '0'))
+	if (!_conv(t->tm_hour, 2, pad))
 	  return 0;
 	continue;
       case 'I':
 	if (!_conv(t->tm_hour % 12 ?
-		   t->tm_hour % 12 : 12, 2, '0'))
+		   t->tm_hour % 12 : 12, 2, pad))
 	  return 0;
 	continue;
       case 'j':
-	if (!_conv(t->tm_yday + 1, 3, '0'))
+	if (!_conv(t->tm_yday + 1, 3, pad))
 	  return 0;
 	continue;
       case 'k':
@@ -127,72 +147,83 @@ _fmt(const char *format, const struct tm *t)
 	  return 0;
 	continue;
       case 'M':
-	if (!_conv(t->tm_min, 2, '0'))
+	if (!_conv(t->tm_min, 2, pad))
 	  return 0;
 	continue;
       case 'm':
-	if (!_conv(t->tm_mon + 1, 2, '0'))
+	if (!_conv(t->tm_mon + 1, 2, pad))
 	  return 0;
 	continue;
       case 'n':
-	if (!_add("\n"))
+	if (!_add("\n", upcase))
 	  return 0;
 	continue;
       case 'p':
-	if (!_add(t->tm_hour >= 12 ? "PM" : "AM"))
+	if (!_add(t->tm_hour >= 12 ? "PM" : "AM", upcase))
 	  return 0;
 	continue;
       case 'R':
-	if (!_fmt("%H:%M", t))
+	if (!_fmt("%H:%M", t, upcase))
 	  return 0;
 	continue;
       case 'r':
-	if (!_fmt("%I:%M:%S %p", t))
+	if (!_fmt("%I:%M:%S %p", t, upcase))
 	  return 0;
 	continue;
       case 'S':
-	if (!_conv(t->tm_sec, 2, '0'))
+	if (!_conv(t->tm_sec, 2, pad))
 	  return 0;
 	continue;
       case 'T':
       case 'X':
-	if (!_fmt("%H:%M:%S", t))
+	if (!_fmt("%H:%M:%S", t, upcase))
 	  return 0;
 	continue;
       case 't':
-	if (!_add("\t"))
+	if (!_add("\t", upcase))
+	  return 0;
+	continue;
+      case 'u':
+	if (!_conv(t->tm_wday==0 ? 7 : t->tm_wday, 1, pad))
 	  return 0;
 	continue;
       case 'U':
 	if (!_conv((t->tm_yday + 7 - t->tm_wday) / 7,
-		   2, '0'))
+		   2, pad))
 	  return 0;
 	continue;
       case 'W':
 	if (!_conv((t->tm_yday + 7 -
 		    (t->tm_wday ? (t->tm_wday - 1) : 6))
-		   / 7, 2, '0'))
+		   / 7, 2, pad))
 	  return 0;
 	continue;
       case 'w':
-	if (!_conv(t->tm_wday, 1, '0'))
+	if (!_conv(t->tm_wday, 1, pad))
 	  return 0;
 	continue;
       case 'x':
-	if (!_fmt("%m/%d/%y", t))
+	if (!_fmt("%m/%d/%y", t, upcase))
 	  return 0;
 	continue;
       case 'y':
-	if (!_conv((t->tm_year + TM_YEAR_BASE)
-		   % 100, 2, '0'))
+      case 'g':
+	if (!_conv((t->tm_year + TM_YEAR_BASE) % 100, 2, pad))
 	  return 0;
 	continue;
       case 'Y':
-	if (!_conv(t->tm_year + TM_YEAR_BASE, 4, '0'))
+      case 'G':
+	if (!_conv(t->tm_year + TM_YEAR_BASE, 4, pad))
+	  return 0;
+	continue;
+      case 'z':
+	if (!_add(t->__tm_gmtoff<0 ? "-" : "+", 0))
+	  return 0;
+	if (!_conv(t->__tm_gmtoff, 4, pad))
 	  return 0;
 	continue;
       case 'Z':
-	if (!t->tm_zone || !_add(t->tm_zone))
+	if (!t->tm_zone || !_add(t->tm_zone, upcase))
 	  return 0;
 	continue;
       case '%':
@@ -204,6 +235,7 @@ _fmt(const char *format, const struct tm *t)
       default:
 	break;
       }
+    }
     if (!gsize--)
       return 0;
     *pt++ = *format;
@@ -217,7 +249,7 @@ strftime(char *s, size_t maxsize, const char *format, const struct tm *t)
   pt = s;
   if ((gsize = maxsize) < 1)
     return 0;
-  if (_fmt(format, t))
+  if (_fmt(format, t, 0))
   {
     *pt = '\0';
     return maxsize - gsize;
