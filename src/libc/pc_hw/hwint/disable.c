9ee1efe08ddb476dccaa4ed1e4cbe13e3a427c22
dj <dj>
1998-01-01 17:56:14 +0000

    import djgpp 2.02

--- src/libc/pc_hw/hwint/disable.c
+++ src/libc/pc_hw/hwint/disable.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #include <dos.h>
 #include <dpmi.h>
@@ -5,5 +6,10 @@
 int
 disable(void)
 {
-  return __dpmi_get_and_disable_virtual_interrupt_state();
+  /* return __dpmi_get_and_disable_virtual_interrupt_state(); */
+  int rv;
+  asm("pushf; popl %0" : "=g" (rv));
+  rv = (rv>>9) & 1;
+  asm("cli");
+  return rv;
 }
--- src/libc/pc_hw/hwint/enable.c
+++ src/libc/pc_hw/hwint/enable.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #include <dos.h>
 #include <dpmi.h>
@@ -5,5 +6,10 @@
 int
 enable(void)
 {
-  return __dpmi_get_and_enable_virtual_interrupt_state();
+  /* return __dpmi_get_and_enable_virtual_interrupt_state(); */
+  int rv;
+  asm("pushf; popl %0" : "=g" (rv));
+  rv = (rv>>9) & 1;
+  asm("sti");
+  return rv;
 }
