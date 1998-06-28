4bb0d17b6248d62d68fea3adec005d76c0bc18ef
dj <dj>
1998-06-28 21:17:44 +0000

    import djgpp 2.02

--- include/sys/fsext.h
+++ include/sys/fsext.h
@@ -1,3 +1,4 @@
+/* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #ifndef __dj_include_sys_fsext_h_
 #define __dj_include_sys_fsext_h_
@@ -24,6 +25,13 @@ typedef enum {
   __FSEXT_close,
   __FSEXT_fcntl,
   __FSEXT_ioctl,
+  __FSEXT_lseek,
+  __FSEXT_link,
+  __FSEXT_unlink,
+  __FSEXT_dup,
+  __FSEXT_dup2,
+  __FSEXT_fstat,
+  __FSEXT_stat
 } __FSEXT_Fnumber;
 
 /* _ready gets passed a fd and should return a mask of these,
@@ -40,6 +48,8 @@ typedef int (__FSEXT_Function)(__FSEXT_Fnumber _function_number,
 int               __FSEXT_alloc_fd(__FSEXT_Function *_function);
 int               __FSEXT_set_function(int _fd, __FSEXT_Function *_function);
 __FSEXT_Function *__FSEXT_get_function(int _fd);
+void             *__FSEXT_set_data(int _fd, void *_data);
+void             *__FSEXT_get_data(int _fd);
 
 int               __FSEXT_add_open_handler(__FSEXT_Function *_function);
 int               __FSEXT_call_open_handlers(__FSEXT_Fnumber _function_number,
