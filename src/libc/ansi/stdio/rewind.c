425ce618804c3457e08da068515e5a8a0a9d9fa4
dj <dj>
1998-06-28 23:45:20 +0000

    import djgpp 2.02

--- src/libc/ansi/stdio/rewind.c
+++ src/libc/ansi/stdio/rewind.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #include <libc/stubs.h>
 #include <stdio.h>
@@ -8,6 +9,7 @@ void rewind(FILE *f)
 {
   fflush(f);
   lseek(fileno(f), 0L, SEEK_SET);
+  f->_fillsize = 512;	/* See comment in filbuf.c */
   f->_cnt = 0;
   f->_ptr = f->_base;
   f->_flag &= ~(_IOERR|_IOEOF);
