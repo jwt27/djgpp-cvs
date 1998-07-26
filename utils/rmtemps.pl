df3e56fea2507e765a74c7a3397a5215df2123e4
dj <dj>
1998-07-26 17:06:54 +0000

    import djgpp 2.02

--- utils/rmtemps.pl
+++ utils/rmtemps.pl
@@ -1,4 +1,4 @@
-#!perl
+#!/bin/perl
 # -*- perl -*-
 
 $rm = 0;
@@ -6,21 +6,23 @@ if ($ARGV[0] eq "-rm") {
     $rm = 1;
     print "Actually removing files...\n";
     shift;
+} else {
+    print "Use -rm to actually remove files\n";
 }
 
 open(F, "find . -type f -print |");
 while (<F>) {
     $t = 0;
-    s/\r//g;
+    s/[\r\n]+//g;
 
     $t = 1 if /\~/;
     $t = 1 if /\#/;
-    $t = 1 if /\/[a-h]{8}$/;
+    $t = 1 if /\.(orig|rej)$/;
+    #$t = 1 if /\/[a-d][a-z]{7}$/ && ! /zoneinfo\//;
 
     if ($t) {
-	print;
+	print $_, "\n";
 	if ($rm) {
-	    s/[\r\n]//g;
 	    unlink($_) || die("unlink($_)");
 	}
     }
