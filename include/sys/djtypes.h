2ec471140f8fec17046e4d286f3ff75c8ba62c6f
dj <dj>
1998-09-09 16:56:26 +0000

    import djgpp 2.02

--- include/sys/djtypes.h
+++ include/sys/djtypes.h
@@ -1,3 +1,4 @@
+/* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #ifndef __DJ_sys_djtypes_h_
 #define __DJ_sys_djtypes_h_
@@ -11,6 +12,15 @@
 #define __DJ_time_t	typedef unsigned int time_t;
 #define __DJ_uid_t	typedef int uid_t;
 #define __DJ_va_list	typedef void *va_list;
-#define __DJ_wchar_t	typedef int wchar_t;
+
+#if defined(__cplusplus) && ( (__GNUC_MINOR__ >= 8 && __GNUC__ == 2 ) || __GNUC__ >= 3 )
+/* wchar_t is now a keyword in C++ */
+#define __DJ_wchar_t
+#else
+/* but remains a typedef in C */
+#define __DJ_wchar_t    typedef int wchar_t;
+#endif
+
+#define __DJ_wint_t     typedef int wint_t;
 
 #endif
