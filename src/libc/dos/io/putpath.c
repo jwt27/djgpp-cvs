c9274d00b04e7f82f2d37dee7b88f6453b9673ce
dj <dj>
1998-10-29 10:24:44 +0000

    import djgpp 2.02

--- src/libc/dos/io/putpath.c
+++ src/libc/dos/io/putpath.c
@@ -1,4 +1,6 @@
+/* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
+#include <libc/stubs.h>
 #include <libc/dosio.h>
 #include <libc/farptrgs.h>
 #include <go32.h>
@@ -15,7 +17,9 @@ _put_path(const char *path)
 void
 _put_path2(const char *path, int offset)
 {
-  int o, space = _go32_info_block.size_of_transfer_buffer - offset;
+  int o = __tb+offset;
+  int space = _go32_info_block.size_of_transfer_buffer - offset;
+  const char *p = path;
 
   if (path == 0)
   {
@@ -23,15 +27,32 @@ _put_path2(const char *path, int offset)
     abort();
   }
 
-  if (strcmp(path, "/dev/null") == 0)
-    path = "nul";
-  if (strcmp(path, "/dev/tty") == 0)
-    path = "con";
-
   _farsetsel(_dos_ds);
 
+  if (p[0] && p[1] == ':')
+    p += 2;
+  if (strncmp(p, "/dev/", 5) == 0)
+  {
+    if (strcmp(p+5, "null") == 0)
+      path = "nul";
+    else if (strcmp(p+5, "tty") == 0)
+      path = "con";
+    else if (((p[5] >= 'a' && p[5] <= 'z')
+	      || (p[5] >= 'A' && p[5] <= 'Z'))
+	     && (p[6] == '/' || p[6] == '\\' || p[6] == '\0'))
+    {
+      /* map /dev/a/ to a:/ */
+      _farnspokeb(o++, p[5]);
+      _farnspokeb(o++, ':');
+      path = p + 6;
+      space -= 2;
+    }
+    else if (p[5])
+      path = p + 5;
+  }
+
   /* collapse multiple slashes to a single slash */
-  for (o=__tb+offset; *path; path++)
+  for (; *path; path++)
   {
     if (path[0] != '/' || path[1] != '/')
     {
