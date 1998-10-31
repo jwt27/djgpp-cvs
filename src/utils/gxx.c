73bf1a82575e8535a8fc915ec75a52154ab22166
dj <dj>
1998-10-31 11:55:46 +0000

    import djgpp 2.02

--- src/utils/gxx.c
+++ src/utils/gxx.c
@@ -1,10 +1,11 @@
+/* Copyright (C) 1997 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1996 DJ Delorie, see COPYING.DJ for details */
 #include <stdio.h>
 #include <process.h>
 #include <stdlib.h>
 
 char lgpp[] = "-lgpp";
-char lstdc[] = "-lstdcx";
+char lstdc[] = "-lstdcxx";
 char lm[] = "-lm";
 
 int
@@ -12,12 +13,21 @@ main(int argc, char **argv)
 {
   char **newargs = (char **)malloc((argc+4) * sizeof(char *));
   int i;
-  for (i=0; i<argc; i++)
-    newargs[i] = argv[i];
+  int add_libs = 1;
 
-  newargs[i++] = lgpp;
-  newargs[i++] = lstdc;
-  newargs[i++] = lm;
+  for (i=0; i<argc; i++)
+  {
+    if (strcmp(argv[i], "-c") == 0
+	|| strcmp(argv[i], "-S") == 0)
+      add_libs = 0;
+    newargs[i] = argv[i];
+  }
+
+  if (add_libs) {
+    newargs[i++] = lstdc;
+    newargs[i++] = lgpp;
+    newargs[i++] = lm;
+  }
   newargs[i++] = 0;
 
   return execvp("gcc.exe", newargs);
