46374e031536cb5c23ae023bfb6cffb6c0d7b9be
dj <dj>
1998-12-02 20:24:44 +0000

    import djgpp 2.02

--- src/libc/ansi/stdlib/rand.c
+++ src/libc/ansi/stdlib/rand.c
@@ -1,13 +1,22 @@
+/* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
+/* Copyright (C) 1996 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #include <stdlib.h>
+#include <pc.h>
+#include <go32.h>
+#include <libc/farptrgs.h>
 
-static unsigned long long next = 0;
+static unsigned long long next = 1;
 
 int
 rand(void)
 {
-  next = next * 0x5deece66dLL + 11;
-  return (int)((next >> 16) & RAND_MAX);
+  /* This multiplier was obtained from Knuth, D.E., "The Art of
+     Computer Programming," Vol 2, Seminumerical Algorithms, Third
+     Edition, Addison-Wesley, 1998, p. 106 (line 26) & p. 108 */
+  next = next * 6364136223846793005LL + 1;
+  /* was: next = next * 0x5deece66dLL + 11; */
+  return (int)((next >> 21) & RAND_MAX);
 }
 
 void
