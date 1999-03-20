dd7f5565ee8bdaeb7f8c878c5c2bafa6f49fedb4
dj <dj>
1999-03-20 22:21:30 +0000

    handle missing files

--- src/utils/bin2h.c
+++ src/utils/bin2h.c
@@ -1,4 +1,6 @@
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
+/* 1998 -- Modified by Nils van den Heuvel (n.heuvel@wxs.nl) */
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -7,16 +9,34 @@
 int
 main(int argc, char **argv)
 {
-  int f = open(argv[1], O_RDONLY|O_BINARY);
-  FILE *of = fopen(argv[3], "w");
+  int f;
+  FILE *of;
   unsigned char buf[4096];
   int rbytes;
   int col=0, i;
   if (argc < 4)
   {
-    printf("usage: bin2byte infile symname outfile\n");
+    printf("usage: %s infile symname outfile\n", basename(argv[0]));
     exit(1);
   }
+  f = open(argv[1], O_RDONLY|O_BINARY);
+  if (f < 1)
+  {
+    printf("%s: ",basename(argv[0]));
+    perror(argv[1]);
+    exit(1);
+  }
+
+  of = fopen(argv[3], "w");
+
+  if (of == NULL)
+  {
+    printf("%s: ",basename(argv[0]));
+    perror(argv[3]);
+    exit(1);
+  }
+
+
   fprintf(of, "unsigned char %s[] = {\n", argv[2]);
   while ((rbytes = read(f, buf, 4096)) > 0)
   {
