4a047cae0f4337d93483130de66bc76af8c81533
dj <dj>
1999-06-03 16:38:32 +0000

    update for 2.03

--- distrib/mkdist.bat
+++ distrib/mkdist.bat
@@ -3,10 +3,10 @@
 cp ../readme.1st ../zips
 cp ../copying* ../zips
 
-call mkdist1 djdev202 Development Kit and Runtime
-call mkdist1 djlsr202 Base Library Sources
-call mkdist1 djtst202 Test Programs
-call mkdist1 djcrx202 For Cross-to-DOS Compiling (subset of djdev)
-call mkdist1 djtzn202 Extra Timezone Support Files
-call mkdist1 djtzs202 Timezone Sources and Utils
+call mkdist1 djdev203 Development Kit and Runtime
+call mkdist1 djlsr203 Base Library Sources
+call mkdist1 djtst203 Test Programs
+call mkdist1 djcrx203 For Cross-to-DOS Compiling (subset of djdev)
+call mkdist1 djtzn203 Extra Timezone Support Files
+call mkdist1 djtzs203 Timezone Sources and Utils
 
