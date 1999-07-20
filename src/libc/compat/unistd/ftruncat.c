ee7a55f8701a94f4a6617454a0801182ed91c4d0
eliz <eliz>
1999-07-20 08:52:40 +0000

    Restore the file pointer position after truncation.  Update the docs.

--- src/libc/compat/unistd/ftruncat.c
+++ src/libc/compat/unistd/ftruncat.c
@@ -6,9 +6,15 @@
 int
 ftruncate(int fd, off_t where)
 {
-  if (lseek(fd, where, 0) == -1)
+  off_t here = lseek(fd, 0, SEEK_CUR);
+  int retval = 0;
+
+  if (here == -1)
+    return -1;
+  if (lseek(fd, where, SEEK_SET) == -1)
     return -1;
   if (_write(fd, 0, 0) < 0)
-    return -1;
-  return 0;
+    retval = -1;
+  lseek(fd, here, SEEK_SET);
+  return retval;
 }
