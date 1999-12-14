2e5dc349e5330eeb25fbbe60dc7a52939842ce4c
eliz <eliz>
1999-12-14 11:57:43 +0000

    Ran copyright.pl before building a pretest.

--- src/libm/math/s_infinity.c
+++ src/libm/math/s_infinity.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 1999 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /*
  * infinity () returns the representation of infinity.
--- src/libm/math/s_signgam.c
+++ src/libm/math/s_signgam.c
@@ -1,2 +1,3 @@
+/* Copyright (C) 1999 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1997 DJ Delorie, see COPYING.DJ for details */
 int signgam = 0;
