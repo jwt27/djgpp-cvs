9ed500d930b990229d838beafa70d09daa9546f1
dj <dj>
2000-07-08 00:21:38 +0000

    div and ldiv return properly signed remainders for negative arguments.

--- src/libc/ansi/stdlib/ldiv.c
+++ src/libc/ansi/stdlib/ldiv.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 2000 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #include <stdlib.h>
 
@@ -6,20 +7,8 @@ ldiv(long num, long denom)
 {
   ldiv_t r;
 
-  if (num > 0 && denom < 0)
-  {
-    num = -num;
-    denom = -denom;
-  }
-  r.quot = num / denom;
   r.rem = num % denom;
-  if (num < 0 && denom > 0)
-  {
-    if (r.rem > 0)
-    {
-      r.quot++;
-      r.rem -= denom;
-    }
-  }
+  r.quot = num / denom;
+
   return r;
 }
