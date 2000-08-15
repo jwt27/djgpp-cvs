2269cde227af267a441ca5534ea4174d1c8f41c3
lauras <lauras>
2000-08-15 17:34:06 +0000

    Testsuite tweaked for __solve_symlinks().

--- tests/libc/compat/unistd/xsymlink.c
+++ tests/libc/compat/unistd/xsymlink.c
@@ -29,7 +29,7 @@ static void test_failure(int num, const char * slink);
 int main(void)
 {
    if (!__file_exists("test1") || !__file_exists("test4") ||
-       !__file_exists("test5") || !__file_exists("test6") ||
+       !__file_exists("test5") || !__file_exists("dirtest") ||
        !__file_exists("fail1") || !__file_exists("fail2") ||
        !__file_exists("fail3") || !__file_exists("dir1/fail1") ||
        !__file_exists("dir1/test1") || !__file_exists("dir1/test2") ||
@@ -44,12 +44,12 @@ int main(void)
    test_success( 1, "test1", "file1");
    test_success( 2, "test4", "file2");
    test_success( 3, "dir1/test1", "dir1/file1");
-   test_success( 4, "test6/file1", "dir1/file1");
-   test_success( 5, "test6/test1", "dir1/file1");
-   test_success( 6, "test6/test2", "/dev/env/DJDIR/bin/gcc.exe");
-   test_success( 7, "test6/test3", "c:\\autoexec.bat");
-   test_success( 8, "test6/test4", "c:/dir/file");
-   test_success( 9, "test6/test6/file", "dir1/dir2/file");
+   test_success( 4, "dirtest/file1", "dir1/file1");
+   test_success( 5, "dirtest/test1", "dir1/file1");
+   test_success( 6, "dirtest/test2", "/dev/env/DJDIR/bin/gcc.exe");
+   test_success( 7, "dirtest/test3", "c:\\autoexec.bat");
+   test_success( 8, "dirtest/test4", "c:/dir/file");
+   test_success( 9, "dirtest/test6/file", "dir1/dir2/file");
    /* Following one returns dir1/../file, which is perfectly valid but
       forces us to use _fixpath before comparisson */
    test_success(10, "dir1/test7", "file");
