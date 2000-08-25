afbd77c660192e3c6ae08fef773aeb27b939f1c0
lauras <lauras>
2000-08-25 11:40:49 +0000

    Added symlink support in rmdir().

--- src/libc/posix/unistd/rmdir.c
+++ src/libc/posix/unistd/rmdir.c
@@ -1,18 +1,25 @@
+/* Copyright (C) 2000 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1996 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #include <libc/stubs.h>
+#include <libc/symlink.h>
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <go32.h>
 #include <dpmi.h>
+#include <stdio.h>
 #include <libc/dosio.h>
 
 int
 rmdir(const char *mydirname)
 {
   __dpmi_regs r;
+  char real_dir[FILENAME_MAX];
+
+  if (!__solve_dir_symlinks(mydirname, real_dir))
+    return -1;
 
   if(_USE_LFN)
     r.x.ax = 0x713a;
@@ -20,7 +27,7 @@ rmdir(const char *mydirname)
     r.h.ah = 0x3a;
   r.x.ds = __tb_segment;
   r.x.dx = __tb_offset;
-  _put_path(mydirname);
+  _put_path(real_dir);
   __dpmi_int(0x21, &r);
 
   if (r.x.flags & 1)
