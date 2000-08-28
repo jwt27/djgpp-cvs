b8dd71dde7b58df3a8577d95141d1f9a40c71b12
lauras <lauras>
2000-08-28 17:49:10 +0000

    Made lchown() resolve symlinks in leading directories.

--- src/libc/compat/unistd/lchown.c
+++ src/libc/compat/unistd/lchown.c
@@ -1,6 +1,8 @@
 /* Copyright (C) 2000 DJ Delorie, see COPYING.DJ for details */
 #include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
+#include <libc/symlink.h>
  
 /* MS-DOS couldn't care less about file ownerships, so we could
    always succeed.  At least fail for non-existent files
@@ -9,7 +11,10 @@
 int
 lchown(const char *path, uid_t owner, gid_t group)
 {
-  if (!__file_exists(path))       /* non-existent file */
+  char real_path[FILENAME_MAX];
+  if (!__solve_dir_symlinks(path, real_path))
+    return -1;
+  if (!__file_exists(real_path))       /* non-existent file */
   {
     errno = ENOENT;
     return -1;
