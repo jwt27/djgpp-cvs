1e08e310842bfda58930fea226ff351eb4711cf0
snowball <snowball>
2001-02-02 00:56:03 +0000

    Add prototype for flock and required macros.

--- include/sys/file.h
+++ include/sys/file.h
@@ -1,3 +1,4 @@
+/* Copyright (C) 2001 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #ifndef __dj_include_sys_file_h_
 #define __dj_include_sys_file_h_
@@ -22,6 +23,18 @@ extern "C" {
 #define L_CURR 1
 #define L_INCR 1
 #define L_XTND 2
+
+/* Operations for the `flock' call.  */
+#define        LOCK_SH 1       /* Shared lock.  */
+#define        LOCK_EX 2       /* Exclusive lock.  */
+#define        LOCK_UN 8       /* Unlock.  */
+
+/* Can be OR'd in to one of the above.  */
+#define        LOCK_NB 4       /* Don't block when locking.  */
+
+/* Apply or remove an advisory lock, according to OPERATION,
+   on the file FD refers to.  */
+int	flock (int _fildes, int _op);
   
 #ifndef __dj_ENFORCE_FUNCTION_CALLS
 #endif /* !__dj_ENFORCE_FUNCTION_CALLS */
