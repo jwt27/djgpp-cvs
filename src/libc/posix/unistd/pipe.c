34667effc7d7cad31e5bbef7f3fda0022ef6f5d7
snowball <snowball>
2001-03-25 18:16:22 +0000

    Implement pipe().

--- src/libc/posix/unistd/pipe.c
+++ src/libc/posix/unistd/pipe.c
@@ -1,10 +1,43 @@
+/* Copyright (C) 2001 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #include <unistd.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <libc/fd_props.h>
 
+/* Emulate a pipe using a temporary file.  */
 int
-pipe(int _fildes[2])
+pipe(int fildes[2])
 {
-  errno = EACCES;
-  return -1;
+  int ifd, ofd;
+  char temp_name[FILENAME_MAX + 1];
+  char *tname;
+
+  tname = tmpnam(temp_name);
+  if (tname == NULL)
+    return -1;
+    
+  ofd = open(tname, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND | O_TEMPORARY,
+             S_IWUSR);
+  if (ofd < 0)
+    return -1;
+    
+  ifd = open(tname, O_RDONLY | O_TEMPORARY);
+  if (ifd < 0)
+  {
+    close(ofd);
+    return -1;
+  }
+
+  if (__has_fd_properties(ofd))
+    __set_fd_flags(ofd, FILE_DESC_PIPE);
+  if (__has_fd_properties(ifd))
+    __set_fd_flags(ifd, FILE_DESC_PIPE);
+
+  fildes[0] = ifd;
+  fildes[1] = ofd;
+    
+  return 0;
 }
