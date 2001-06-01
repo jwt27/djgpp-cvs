ddad0a54b5decc6c0a3add6a2702894431e2f926
eliz <eliz>
2001-06-01 14:49:21 +0000

    Add a new member d_type to struct dirent.  Compute d_type in readdir. Add definitions of D_* and DTTOIF macros to dirent.h.

--- src/libc/posix/dirent/readdir.c
+++ src/libc/posix/dirent/readdir.c
@@ -1,13 +1,17 @@
+/* Copyright (C) 2001 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1997 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #include <libc/stubs.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <dirent.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <ctype.h>
+#include <io.h>
+#include <libc/symlink.h>
 #include "dirstruc.h"
 
 struct dirent *
@@ -26,6 +30,10 @@ readdir(DIR *dir)
     else
       strcpy(dir->de.d_name, "..");
     dir->de.d_namlen = strlen(dir->de.d_name);
+    if ((__opendir_flags & __OPENDIR_NO_D_TYPE) == 0)
+      dir->de.d_type = DT_DIR;
+    else
+      dir->de.d_type = DT_UNKNOWN;
     return &(dir->de);
   }
 
@@ -71,6 +79,43 @@ readdir(DIR *dir)
   }
   strcpy(dir->de.d_name, dir->ff.ff_name);
   dir->de.d_namlen = strlen(dir->de.d_name);
+  if ((__opendir_flags & __OPENDIR_NO_D_TYPE) == 0)
+    {
+      unsigned char attrib = dir->ff.ff_attrib;
+
+      if ((attrib & FA_DIREC) == FA_DIREC)
+	dir->de.d_type = DT_DIR;
+      else if ((attrib & FA_LABEL) == FA_LABEL)
+	dir->de.d_type = DT_LABEL;
+      else if ((attrib & 0x40) == 0x40)
+	dir->de.d_type = DT_CHR;
+      else if (dir->ff.ff_fsize == _SYMLINK_FILE_LEN)
+      {
+	int fhandle = -1;
+	char fname[FILENAME_MAX];
+
+	dir->de.d_type = DT_REG;
+	strcat(strcat(strcpy(fname, dir->name), "/"), dir->de.d_name);
+	/* Opening a file is expensive, but hopefully there won't be
+	   so many files whose length is 510 bytes.  */
+	fhandle = _open(fname, O_RDONLY);
+	if (fhandle < 0)
+	  fhandle = _open(fname, O_RDONLY | SH_DENYNO);
+	if (fhandle >= 0)
+	{
+	  char sbuf[_SYMLINK_FILE_LEN];
+
+	  if (__internal_readlink(NULL, fhandle, sbuf, _SYMLINK_FILE_LEN) > 0)
+	    dir->de.d_type = DT_LNK;
+	  _close(fhandle);
+	}
+      }
+      /* FIXME: anything else DOS can return? */
+      else
+	dir->de.d_type = DT_REG;
+    }
+  else
+    dir->de.d_type = DT_UNKNOWN;
   return &dir->de;
 }
 
