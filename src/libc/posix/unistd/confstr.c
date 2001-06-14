7ceba408f949dafb0e6ae0f6016d695cfea8dfae
snowball <snowball>
2001-06-14 02:55:50 +0000

    New file. Implements confstr().

---
+++ src/libc/posix/unistd/confstr.c
@@ -0,0 +1,47 @@
+/* Copyright (C) 2001 DJ Delorie, see COPYING.DJ for details */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+static char *startup_djdir;
+
+static void __attribute__((constructor))
+init_confstr(void)
+{
+  char *djdir = getenv("DJDIR");
+  if (djdir)
+  {
+    startup_djdir = malloc(strlen(djdir) + 1);
+    if (startup_djdir)
+      strcpy(startup_djdir, djdir);
+  }
+}
+
+size_t
+confstr(int name, char *buf, size_t len)
+{
+  size_t out_len = 0;
+
+  switch (name)
+  {
+    case _CS_PATH:
+    {
+      if (startup_djdir)
+      {
+        out_len = snprintf(buf, len, "%s/bin", startup_djdir);
+        /* snprintf excludes the null terminator from its return value,
+           but confstr includes it.  */
+        ++out_len;
+      }
+      break;
+    }
+    default:
+    {
+      errno = EINVAL;
+    }
+  }
+  return out_len;
+}
+
