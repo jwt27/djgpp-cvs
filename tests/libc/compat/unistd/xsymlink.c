ba127abdcdf0ab136adaceef5dfd0905fc7a97f1
lauras <lauras>
2001-06-27 17:43:53 +0000

    Deleted broken test and other minor tweaks.

--- tests/libc/compat/unistd/xsymlink.c
+++ tests/libc/compat/unistd/xsymlink.c
@@ -7,14 +7,17 @@
  *   5. Symlink in symlinked directory
  *   6. Symlink in symlinked directory to UNIX-style absolute path
  *   7. The same with DOS-style absolute path
- *   8. Symlink in symlinked directory to absolute path with symlinks itself
- *   9. Real file in a symlink subdir in a symlink subdir
- *  10. Symlink in a subdir to file in an upper dir
+ *   8. Real file in a symlink subdir in a symlink subdir
+ *   9. Symlink in a subdir to file in an upper dir
  * Any unhandled cases are more than welcome.
  *
  * And following are failure tests:
  *  11. Simple symlink loop.
  *  12. Symlink loop across directories
+ *
+ *     TODO: device a test, where symlink expands to absolute path, which,
+ *  in turn, has symlinks somewhere inside. There was such a test once, but
+ *  it was broken - it depended on testsuite location.
  */
 #include <errno.h>
 #include <stdio.h>
@@ -23,8 +26,10 @@
 #include <libc/symlink.h>
 #include <sys/stat.h>
 
-static void test_success(int num, const char * slink, const char * expect);
-static void test_failure(int num, const char * slink);
+static void test_success(const char * slink, const char * expect);
+static void test_failure(const char * slink);
+
+static int test_num = 0;
 
 int main(void)
 {
@@ -41,25 +46,22 @@ int main(void)
        exit(1);
    }
    printf("Running readlink() testsuite:\n");
-   test_success( 1, "test1", "file1");
-   test_success( 2, "test4", "file2");
-   test_success( 3, "dir1/test1", "dir1/file1");
-   test_success( 4, "dirtest/file1", "dir1/file1");
-   test_success( 5, "dirtest/test1", "dir1/file1");
-   test_success( 6, "dirtest/test2", "/dev/env/DJDIR/bin/gcc.exe");
-   test_success( 7, "dirtest/test3", "c:\\autoexec.bat");
-   test_success( 8, "dirtest/test4", "c:/dir/file");
-   test_success( 9, "dirtest/test6/file", "dir1/dir2/file");
-   /* Following one returns dir1/../file, which is perfectly valid but
-      forces us to use _fixpath before comparisson */
-   test_success(10, "dir1/test7", "file");
-   test_failure(11, "fail1");
-   test_failure(12, "fail3");
+   test_success("test1", "file1");
+   test_success("test4", "file2");
+   test_success("dir1/test1", "dir1/file1");
+   test_success("dirtest/file1", "dir1/file1");
+   test_success("dirtest/test1", "dir1/file1");
+   test_success("dirtest/test2", "/dev/env/DJDIR/bin/gcc.exe");
+   test_success("dirtest/test3", "c:\\autoexec.bat");
+   test_success("dirtest/test6/file", "dir1/dir2/file");
+   test_success("dir1/test7", "file");
+   test_failure("fail1");
+   test_failure("fail3");
    printf("Done.\n");
    return 0;
 }
 
-static void test_success(int num, const char * slink, const char * expect)
+static void test_success(const char * slink, const char * expect)
 {
    char real_name[FILENAME_MAX + 1];
    char real_fixed[FILENAME_MAX + 1];
@@ -67,7 +69,7 @@ static void test_success(int num, const char * slink, const char * expect)
    char err_buf[50];
    if (!__solve_symlinks(slink, real_name))
    {
-      sprintf(err_buf, "Test %d failed ", num);
+      sprintf(err_buf, "Test %d failed ", ++test_num);
       perror(err_buf);
       exit(1);
    }
@@ -77,13 +79,15 @@ static void test_success(int num, const char * slink, const char * expect)
    {
       fprintf(stderr,
             "Test %d failed - __solve_symlinks returns wrong resolved path\n",
-            num);
+            test_num);
+      fprintf(stderr, "Returned path: %s\n", real_fixed);
+      fprintf(stderr, "Expected path: %s\n", expect_fixed);
       exit(1);
    }
-   printf("Test %d passed\n", num);
+   printf("Test %d passed\n", test_num);
 }
 
-static void test_failure(int num, const char * slink)
+static void test_failure(const char * slink)
 {
    char buf[FILENAME_MAX + 1];
    char err_buf[50];
@@ -92,14 +96,14 @@ static void test_failure(int num, const char * slink)
    {
       fprintf(stderr,
             "Test %d failed - __solve_symlinks suceeds when it should fail\n",
-            num);
+            ++test_num);
       exit(1);
    }
    if (errno != ELOOP)
    {
-      sprintf(err_buf, "Test %d failed - wrong errno returned ", num);
+      sprintf(err_buf, "Test %d failed - wrong errno returned ", test_num);
       perror(err_buf);
       exit(1);
    }
-   printf("Test %d passed\n", num);
+   printf("Test %d passed\n", test_num);
 }
