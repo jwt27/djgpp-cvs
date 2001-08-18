ae7a6750694f0147121fe3fc12a9b6ef3e72147f
sandmann <sandmann>
2001-08-18 03:53:35 +0000

    Add lots of ugly code to attempt to make _open handles work with Windows 2000 and XP.  Long file name handles don't work well with IOCTL calls, so fstat fails.  Convert name to short and open that way. Special handling for devices, since short name conversions don't work.

--- src/libc/dos/io/_open.c
+++ src/libc/dos/io/_open.c
@@ -28,6 +28,48 @@ _open(const char* filename, int oflag)
   if (__FSEXT_call_open_handlers(__FSEXT_open, &rv, &filename))
     return rv;
 
+  if(use_lfn && _osmajor == 5 && _get_dos_version(1) == 0x532) {
+    /* Windows 2000 or XP; or NT with LFN TSR.  Windows 2000 behaves
+       badly when using IOCTL and write-truncate calls on LFN handles.
+       We convert the long name to a short name and open existing files
+       via short name.  New files use LFN, but we know they aren't
+       character devices. */
+    r.x.ax = 0x7160;
+    r.x.cx = 1;				/* Get short name equivalent */
+    r.x.ds = __tb_segment;
+    r.x.si = __tb_offset;		/* Long name to convert - putpath */
+    r.x.es = __tb_segment;
+    r.x.di = __tb_offset + _put_path(filename);	/* Short name destination */
+    __dpmi_int(0x21, &r);
+    if(!(r.x.flags & 1)) {		/* Get short name success */
+      r.x.ax = 0x6c00;
+      r.x.bx = (oflag & 0xff);
+      r.x.dx = 1;			/* Open existing file */
+      r.x.si = r.x.di;
+      goto do_open;
+    } else {
+      /* Short name get failed, file doesn't exist or is device (same error) */
+      r.x.ax = 0x7143;			/* Get attributes */
+      r.h.bl = 0;
+      r.x.dx = __tb_offset;		/* Original long name */
+      __dpmi_int(0x21, &r);		/* This is same as lfn _chmod */
+      if(!(r.x.flags & 1)) {		/* Name exists, probably device */
+        r.x.ax = 0x6c00;
+        r.x.bx = (oflag & 0xff);
+        r.x.dx = 1;                     /* Open existing file */
+        r.x.si = __tb_offset;		/* Treat original name as short */
+        r.x.cx = 0;
+        __dpmi_int(0x21, &r);
+        if(!(r.x.flags & 1)) {		/* Success! */
+          goto do_hset;
+        }
+	/* Fail on short name open after _chmod said OK.
+	   Device with directory?  We should re-try with LFN.
+	   Permission?  Readonly file?  We should quit.
+	   Let it fall through to the LFN open which should succeed.  */
+      }
+    }
+  }
   if(use_lfn) {
     r.x.ax = 0x716c;
     r.x.bx = (oflag & 0xff);
@@ -54,15 +96,17 @@ _open(const char* filename, int oflag)
       r.x.dx = __tb_offset;
     }
   }
-  r.x.cx = 0;
   r.x.ds = __tb_segment;
   _put_path(filename);
+do_open:
+  r.x.cx = 0;
   __dpmi_int(0x21, &r);
   if(r.x.flags & 1)
   {
     errno = __doserr_to_errno(r.x.ax);
     return -1;
   }
+do_hset:
   __file_handle_set(r.x.ax, O_BINARY);
   return r.x.ax;
 }
