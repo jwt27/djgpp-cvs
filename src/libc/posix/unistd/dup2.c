b709c16929b117f79bccfedf9a216480f1867069
sandmann <sandmann>
2001-12-05 18:26:27 +0000

    v2.03 refresh - Eli fsext bug fix from revision 1.5

--- src/libc/posix/unistd/dup2.c
+++ src/libc/posix/unistd/dup2.c
@@ -9,7 +9,6 @@
 #include <io.h>
 #include <sys/fsext.h>
 #include <libc/dosio.h>
-#include <libc/fd_props.h>
 
 int
 dup2(int fd, int newfd)
@@ -17,7 +16,7 @@ dup2(int fd, int newfd)
   __dpmi_regs r;
   if (fd == newfd)
     return newfd;
-  /* Reset the text/binary modes, the fsext, and the properties, since
+  /* Reset the text/binary modes and the fsext, since
      DOS function 46h closes newfd.  */
   __file_handle_set(newfd, __file_handle_modes[fd] ^ (O_BINARY|O_TEXT));
   __FSEXT_set_function(newfd, 0);
@@ -31,18 +30,8 @@ dup2(int fd, int newfd)
     return -1;
   }
 
-  /* The properties may be cleared only after the file has been
-     forcibly closed because the file associated with the
-     descriptor may be deleted if it has the temporary attribute
-     and if newfd is the last reference to it.  */
-  if (__has_fd_properties(newfd))
-    __clear_fd_properties(newfd);
-
-  /* Copy the fsext hook, the handle modes, and the properties.  */
+  /* Copy the fsext hook and the handle modes.  */
   __FSEXT_set_function(newfd, __FSEXT_get_function(fd));
   setmode(newfd, __file_handle_modes[fd]);
-  if (__has_fd_properties(fd))
-    __dup_fd_properties(fd, newfd);
-
   return newfd;
 }
