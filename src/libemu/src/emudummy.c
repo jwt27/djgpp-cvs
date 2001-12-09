bfb1c99359a60ba4631b0260922abdbd2dd98a99
richdawe <richdawe>
2001-12-09 20:18:07 +0000

    Fix emu387.dxe build, when building against profiled libc.

--- src/libemu/src/emudummy.c
+++ src/libemu/src/emudummy.c
@@ -1,14 +1,39 @@
+/* Copyright (C) 2001 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1997 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1996 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
-/* Dummy routines to enable building of emu387.dxe from libemu.a - since we
-   can't do I/O, dummy the I/O routines out.  */
-   
-void vsprintf(void);
-void _write(void);
 
-void vsprintf(void) {}
-void _write(void) {}
+/* Dummy routines to enable building of emu387.dxe from libemu.a.
+ *
+ * Since we can't do I/O, dummy the I/O routines out. Since we can't link
+ * against a profiled libc, dummy the profiling functions out.
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <io.h>
+#include <libc/dosio.h>
+#include <errno.h>
+   
+int
+vsprintf (char *buf, const char *fmt, va_list args)
+{
+  return 0;
+}
+
+ssize_t
+_write (int fd, const void *buf, size_t nbyte)
+{
+  errno = EACCES;
+  return -1;
+}
+
+void mcount (int _to);
+
+void
+mcount (int _to)
+{
+}
 
 int __djgpp_exception_state_ptr;
    
