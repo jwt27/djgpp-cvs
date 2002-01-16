f833c3c33866c3a5add6624409149f1b57e6a72f
sandmann <sandmann>
2002-01-16 04:09:59 +0000

    Do not assume handles 0..4 are open when assigning them to standard FILE structures.  Fixes problem with flushing of files on exit.

--- src/libc/ansi/stdio/frlist.c
+++ src/libc/ansi/stdio/frlist.c
@@ -1,6 +1,8 @@
 /* Copyright (C) 1999 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #include <stdio.h>
+#include <io.h>
+#include <libc/file.h>
 #include <libc/local.h>
 
 static __file_rec __initial_file_rec;
@@ -21,4 +23,28 @@ __setup_file_rec_list(void)
   __initial_file_rec.files[2] = stderr;
   __initial_file_rec.files[3] = stdprn;	/* in reverse order! (history) */
   __initial_file_rec.files[4] = stdaux;
+
+  /* A parent program may have closed one or more of the standard 0..4 DOS 
+     handles.  If so, we disable the FILE structure so I/O will fail.
+     The exit handler may close this structure, but since the handle is
+     impossible it will not cause flushing problems with a new file which
+     may have been opened on that handle.  If the FILE structure is
+     freopened it will flush OK on exit.
+     
+     Note: fileno() is a macro, so can be on the LHS of assignment. */
+
+  if(_get_dev_info(fileno(stdin)) == -1)
+    fileno(stdin) = -1;
+
+  if(_get_dev_info(fileno(stdout)) == -1)
+    fileno(stdout) = -1;
+
+  if(_get_dev_info(fileno(stderr)) == -1)
+    fileno(stderr) = -1;
+
+  if(_get_dev_info(fileno(stdprn)) == -1)
+    fileno(stdprn) = -1;
+
+  if(_get_dev_info(fileno(stdaux)) == -1)
+    fileno(stdaux) = -1;
 }
