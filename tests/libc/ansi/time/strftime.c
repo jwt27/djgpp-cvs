62ea416c7087e2e6005c7d4274775e3ab5cb8a49
richdawe <richdawe>
2002-05-26 16:12:19 +0000

    Include <string.h>, to get prototype for strcmp().

--- tests/libc/ansi/time/strftime.c
+++ tests/libc/ansi/time/strftime.c
@@ -6,6 +6,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/time.h>
 #include <time.h>
 #include <libc/unconst.h>
--- tests/libc/compat/unistd/xsymlink.c
+++ tests/libc/compat/unistd/xsymlink.c
@@ -22,6 +22,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <libc/symlink.h>
 #include <sys/stat.h>
