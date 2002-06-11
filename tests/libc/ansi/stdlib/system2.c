1ef1a8b395872b31196c7579f58153e3910f397b
richdawe <richdawe>
2002-06-11 10:27:03 +0000

    Use proper form of multi-line string, not the form that's been deprecated in gcc 3.x. gcc 3.1 warns about the old usage, which will not be supported in future gcc versions.

--- tests/libc/ansi/stdlib/system2.c
+++ tests/libc/ansi/stdlib/system2.c
@@ -9,25 +9,25 @@ main (int argc, char **argv)
   if (argc <= 1)
     {
       /* Assuming we have a djecho.exe: */
-      res = system ("djecho
-		William Safire's Rules for Writers:
-
-Remember to never split an infinitive. The passive voice should never be used.
-Do not put statements in the negative form. Verbs have to agree with their
-subjects. Proofread carefully to see if you words out. If you reread your work,
-you can find on rereading a great deal of repetition can be avoided by
-rereading and editing. A writer must not shift your point of view. And don't
-start a sentence with a conjunction. (Remember, too, a preposition is a
-terrible word to end a sentence with.)  Don't overuse exclamation marks!!
-Place pronouns as close as possible, especially in long sentences, as of 10
-or more words, to their antecedents. Writing carefully, dangling participles
-must be avoided. If any word is improper at the end of a sentence, a linking
-verb is. Take the bull by the hand and avoid mixing metaphors. Avoid trendy
-locutions that sound flaky. Everyone should be careful to use a singular
-pronoun with singular nouns in their writing. Always pick on the correct idiom.
-The adverb always follows the verb. Last but not least, avoid cliches like the
-plague; seek viable alternatives.
-> rules.tmp");
+      res = system ("djecho \
+\"		William Safire\'s Rules for Writers:\n\
+\n\
+Remember to never split an infinitive. The passive voice should never be used.\n\
+Do not put statements in the negative form. Verbs have to agree with their\n\
+subjects. Proofread carefully to see if you words out. If you reread your work,\n\
+you can find on rereading a great deal of repetition can be avoided by\n\
+rereading and editing. A writer must not shift your point of view. And don\'t\n\
+start a sentence with a conjunction. (Remember, too, a preposition is a\n\
+terrible word to end a sentence with.)  Don\'t overuse exclamation marks!!\n\
+Place pronouns as close as possible, especially in long sentences, as of 10\n\
+or more words, to their antecedents. Writing carefully, dangling participles\n\
+must be avoided. If any word is improper at the end of a sentence, a linking\n\
+verb is. Take the bull by the hand and avoid mixing metaphors. Avoid trendy\n\
+locutions that sound flaky. Everyone should be careful to use a singular\n\
+pronoun with singular nouns in their writing. Always pick on the correct idiom.\n\
+The adverb always follows the verb. Last but not least, avoid cliches like the\n\
+plague; seek viable alternatives.\"\
+ > rules.tmp");
       if (!res)
 	res = system ("sed s/s/z/g < rules.tmp | tail");
     }
