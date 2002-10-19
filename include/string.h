619fdc799e534c61bfe3617752f98cc20cfedff2
sandmann <sandmann>
2002-10-19 21:52:13 +0000

    Fix prototype for bzero to fix problems with GCC 3.x (part of refresh).

--- include/string.h
+++ include/string.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2000 DJ Delorie, see COPYING.DJ for details */
+/* Copyright (C) 2002 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #ifndef __dj_include_string_h_
@@ -16,11 +16,9 @@ extern "C" {
 #undef NULL
 
 #define NULL 0
-
-#ifndef _SIZE_T
 __DJ_size_t
-#define _SIZE_T
-#endif
+#undef __DJ_size_t
+#define __DJ_size_t
 
 void *	memchr(const void *_s, int _c, size_t _n);
 int	memcmp(const void *_s1, const void *_s2, size_t _n);
@@ -52,15 +50,18 @@ size_t	strxfrm(char *_s1, const char *_s2, size_t _n);
 #include <sys/movedata.h>
 
 int	bcmp(const void *_ptr1, const void *_ptr2, int _length);
-void 	bcopy(const void *_a, void *_b, size_t _len);
-void 	bzero(void *ptr, size_t _len);
+void *	bcopy(const void *_a, void *_b, size_t _len);
+#if __GNUC__ >= 3
+#define bzero(s, n) __builtin_bzero(s, n)
+#else
+void *	bzero(void *ptr, size_t _len);
+#endif
 int	ffs(int _mask);
 char *  index(const char *_string, int _c);
 void *	memccpy(void *_to, const void *_from, int c, size_t n);
 int	memicmp(const void *_s1, const void *_s2, size_t _n);
 char *  rindex(const char *_string, int _c);
 char *	stpcpy(char *_dest, const char *_src);
-char *	stpncpy(char *_dest, const char *_src, size_t _n);
 char *	strdup(const char *_s);
 char *	strlwr(char *_s);
 int	strcasecmp(const char *_s1, const char *_s2);
