df04e6169abf3b82243d0317981e7c4982462031
richdawe <richdawe>
2002-11-25 19:58:42 +0000

    Use plain old 'echo' rather than 'becho'; if a DSM exists, include it in the distribution.

--- distrib/mkdist1.bat
+++ distrib/mkdist1.bat
@@ -1,15 +1,15 @@
 @echo off
 
-becho Making %1... 
+echo Making %1... 
 cd ..
 
 echo %1 %2 %3 %4 %5 %6 %7 %8 %9 > manifest\%1.ver
 
-becho find... 
+echo find... 
 find @distrib/p/%1/files -type f -print > manifest\%1.mft
 
 if not exist distrib\p\%1\skips goto noskip
-becho skip... 
+echo skip... 
 sed -f distrib/p/%1/skips manifest/%1.mft > manifest\%1.tmp
 mv manifest/%1.tmp manifest/%1.mft
 :noskip
@@ -17,18 +17,24 @@ mv manifest/%1.tmp manifest/%1.mft
 echo manifest/%1.mft>>manifest\%1.mft
 echo manifest/%1.ver>>manifest\%1.mft
 
+if not exist distrib\p\%1\%1.dsm goto nodsm
+echo dsm...
+cp distrib\p\%1\%1.dsm manifest/%1.dsm
+echo manifest/%1.dsm>>manifest\%1.mft
+:nodsm
+
 if not exist distrib\p\%1\nobins goto dobins
-becho nobins... 
+echo nobins... 
 cp manifest/%1.mft manifest/%1.tmp
 distrib\nobins @distrib/p/%1/nobins < manifest\%1.tmp > manifest\%1.tm2
 mv manifest/%1.tm2 manifest/%1.mft
 :dobins
 
-becho sort... 
+echo sort... 
 sort < manifest\%1.mft > manifest\%1.tmp
 cp manifest/%1.tmp manifest/%1.mft
 
-becho zip... 
+echo zip... 
 if exist zips\%1.zip del zips\%1.zip
 zip -qq -9 -@ zips/%1.zip < manifest\%1.tmp
 rm manifest/%1.tmp
