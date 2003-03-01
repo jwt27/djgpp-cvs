ffd48a790d706c027d7b50973b6570848b28c3e9
richdawe <richdawe>
2003-03-01 13:17:36 +0000

    Add a test for isatty()

---
+++ tests/libc/posix/unistd/t-isatty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+
+int
+main (void)
+{
+  /* Check the standard handles */
+  assert(isatty(fileno(stdin)));
+  assert(isatty(fileno(stdout)));
+  assert(!isatty(fileno(stdprn)));
+
+  /* Check an invalid file descriptor. */
+  assert(isatty(99) < 0);
+
+  puts("PASS");
+  return(EXIT_SUCCESS);
+}
