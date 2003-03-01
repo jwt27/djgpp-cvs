13f42ec4105f714dd7300a5335e5d4acbcbaeae2
richdawe <richdawe>
2003-03-01 13:19:12 +0000

    Make isatty use _get_dev_info and the defines from <libc/getdinfo.h>. A benefit of this is that isatty() will now fail for invalid descriptors, as required by POSIX.

--- src/libc/posix/unistd/isatty.c
+++ src/libc/posix/unistd/isatty.c
@@ -1,16 +1,21 @@
+/* Copyright (C) 2003 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #include <libc/stubs.h>
 #include <unistd.h>
-#include <dpmi.h>
+#include <io.h>
+#include <libc/getdinfo.h>
+
+#define ISATTY_BITS (_DEV_CDEV|_DEV_STDIN|_DEV_STDOUT)
 
 int
 isatty(int fd)
 {
-  __dpmi_regs r;
-  r.x.ax = 0x4400;
-  r.x.bx = fd;
-  __dpmi_int(0x21, &r);
-  if ((r.x.ax & 0x83) == 0x83)
+  const int dev_info = _get_dev_info(fd);
+
+  if (dev_info == -1)
+    return -1;
+
+  if ((dev_info & ISATTY_BITS) == ISATTY_BITS)
     return 1;
   return 0;
 }
