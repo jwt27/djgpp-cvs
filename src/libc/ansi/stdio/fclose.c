ace549b4310a854603bffe2aa0391014090f804e
richdawe <richdawe>
2003-03-26 19:50:11 +0000

    Tidy up FILEs for directories too.

--- src/libc/ansi/stdio/fclose.c
+++ src/libc/ansi/stdio/fclose.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 2003 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #include <libc/stubs.h>
 #include <stdio.h>
@@ -6,30 +7,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <libc/file.h>
+#include <libc/fd_props.h>
 
 int
 fclose(FILE *f)
 {
-  int r;
+  const int fd = fileno(f);
+  int r = EOF;
 
-  r = EOF;
   if (!f)
     return r;
-  if (f->_flag & (_IOREAD|_IOWRT|_IORW)
-      && !(f->_flag&_IOSTRG))
+
+  /* A FILE for a directory won't have any of the read or write flags
+   * set. But we still want to tidy it up. */
+  if (   (f->_flag & (_IOREAD|_IOWRT|_IORW) && !(f->_flag&_IOSTRG))
+      || (__get_fd_flags(fd) & FILE_DESC_DIRECTORY))
   {
     r = fflush(f);
-    if (close(fileno(f)) < 0)
+    if (close(fd) < 0)
       r = EOF;
     if (f->_flag&_IOMYBUF)
       free(f->_base);
   }
+
   if (f->_flag & _IORMONCL && f->_name_to_remove)
   {
     remove(f->_name_to_remove);
     free(f->_name_to_remove);
     f->_name_to_remove = 0;
   }
+
   f->_cnt = 0;
   f->_base = 0;
   f->_ptr = 0;
