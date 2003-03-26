603b3abfc80a711ab4a9183089187eda9821e515
richdawe <richdawe>
2003-03-26 19:49:37 +0000

    Maintain EOF for FILEs for directories, so that I/O fails for directories.

--- src/libc/ansi/stdio/rewind.c
+++ src/libc/ansi/stdio/rewind.c
@@ -1,14 +1,23 @@
+/* Copyright (C) 2003 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #include <libc/stubs.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <libc/file.h>
+#include <libc/fd_props.h>
 
 void rewind(FILE *f)
 {
+  const int fd = fileno(f);
+
+  /* If this is a FILE for a directory, we must maintain its EOF flag.
+   * Just return. */
+  if (__get_fd_flags(fd) & FILE_DESC_DIRECTORY)
+    return;
+
   fflush(f);
-  lseek(fileno(f), 0L, SEEK_SET);
+  lseek(fd, 0L, SEEK_SET);
   f->_fillsize = 512;	/* See comment in filbuf.c */
   f->_cnt = 0;
   f->_ptr = f->_base;
