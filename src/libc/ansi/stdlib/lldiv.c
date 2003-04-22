19ce721b78f4a78e6e5e953bea426d7b0e5ae612
richdawe <richdawe>
2003-04-22 21:42:51 +0000

    lldiv returns properly signed remainders for negative arguments.

--- src/libc/ansi/stdlib/lldiv.c
+++ src/libc/ansi/stdlib/lldiv.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 2003 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 2002 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1996 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
@@ -8,20 +9,8 @@ lldiv(long long int num, long long int denom)
 {
   lldiv_t r;
 
-  if (num > 0 && denom < 0)
-  {
-    num = -num;
-    denom = -denom;
-  }
-  r.quot = num / denom;
   r.rem = num % denom;
-  if (num < 0 && denom > 0)
-  {
-    if (r.rem > 0)
-    {
-      r.quot++;
-      r.rem -= denom;
-    }
-  }
+  r.quot = num / denom;
+
   return r;
 }
