68bff9223c13a0623c6c77c83db280b36d4abcf7
peuha <peuha>
2003-11-27 09:32:57 +0000

    Make assert print function name for C99 compliance.

--- src/libc/ansi/assert/assert.c
+++ src/libc/ansi/assert/assert.c
@@ -4,10 +4,13 @@
 #include <signal.h>
 #include <stdlib.h>
 
-void	__dj_assert(const char *msg, const char *file, int line)
+void	__dj_assert(const char *msg, const char *file, int line, const char *func)
 {
-  /* Assertion failed at foo.c line 45: x<y */
-  fprintf(stderr, "Assertion failed at %s line %d: %s\n", file, line, msg);
+  /* Assertion failed at foo.c line 45, function bar: x<y */
+  fprintf(stderr, "Assertion failed at %s line %d", file, line);
+  if(func)
+    fprintf(stderr, ", function %s", func);
+  fprintf(stderr, ": %s\n", msg);
   raise(SIGABRT);
   exit(1);
 }
