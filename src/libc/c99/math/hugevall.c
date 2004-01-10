c1265e6c30f82534990d0515396aa46b7d640f4e
ams <ams>
2004-01-10 16:45:18 +0000

    Make it a proper infinity instead of an unnormal.

--- src/libc/c99/math/hugevall.c
+++ src/libc/c99/math/hugevall.c
@@ -1,4 +1,4 @@
 /* Copyright (C) 2003 DJ Delorie, see COPYING.DJ for details */
 #include <libc/ieee.h>
 
-long_double_t __dj_huge_vall = { 0x00000000, 0x00000000, 0x7fff, 0x0 };
+long_double_t __dj_huge_vall = { 0x00000000, 0x80000000, 0x7fff, 0x0 };
