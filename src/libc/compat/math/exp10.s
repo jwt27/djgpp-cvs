277f47921efb76bb52a58f0b1b2ae80b9105e2df
juan.guerrero <juan.guerrero>
2011-04-17 18:55:47 +0000

    Replace spaces with tabs and update copywrite year.

--- src/libc/compat/math/exp10.s
+++ src/libc/compat/math/exp10.s
@@ -1,5 +1,6 @@
+/* Copyright (C) 2011 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1999 DJ Delorie, see COPYING.DJ for details */
-	.file "exp10.s"
-	.global _exp10
+	.file	"exp10.s"
+	.global	_exp10
 _exp10:
-	jmp ___pow10
+	jmp	___pow10
--- src/libc/compat/math/exp2.s
+++ src/libc/compat/math/exp2.s
@@ -1,5 +1,6 @@
+/* Copyright (C) 2011 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1999 DJ Delorie, see COPYING.DJ for details */
-	.file "exp2.s"
-	.global _exp2
+	.file	"exp2.s"
+	.global	_exp2
 _exp2:
-	jmp ___pow2
+	jmp	___pow2
