7388cf880e6e3a094407baf1e08dcf89a7670644
juan.guerrero <juan.guerrero>
2011-12-21 18:24:11 +0000

    Use const qualifier.

--- src/libc/crt0/rfinfo.c
+++ src/libc/crt0/rfinfo.c
@@ -1,7 +1,8 @@
+/* Copyright (C) 2011 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1998 DJ Delorie, see COPYING.DJ for details */
-void __register_frame_info(void *begin __attribute__((unused)),
-                           void *object __attribute__((unused)));
-void __register_frame_info(void *begin __attribute__((unused)),
-                           void *object __attribute__((unused)))
+void __register_frame_info(const void *begin __attribute__((unused)),
+                           const void *object __attribute__((unused)));
+void __register_frame_info(const void *begin __attribute__((unused)),
+                           const void *object __attribute__((unused)))
 {
 }
