faa1db5624e8b0c0dfdeef41fb4ff65758030ed7
juan.guerrero <juan.guerrero>
2012-12-08 22:11:43 +0000

    Pacify compiler: -Werror=unused-but-set-variable.

--- tests/libc/ansi/stdlib/mbstowcs.c
+++ tests/libc/ansi/stdlib/mbstowcs.c
@@ -2,11 +2,18 @@
 
 int main(void)
 {
+#if 0
   size_t len;
+#endif
 
   char s[] = "A";
 
+/* Pacify compiler: -Werror=unused-but-set-variable  */
+#if 0
   len = mbstowcs(NULL, s, 0);
+#else
+  mbstowcs(NULL, s, 0);
+#endif
 
   return 0;
 }
--- tests/libc/ansi/stdlib/wcstombs.c
+++ tests/libc/ansi/stdlib/wcstombs.c
@@ -2,11 +2,18 @@
 
 int main(void)
 {
+#if 0
   size_t len;
+#endif
 
   wchar_t ws[] = L"A";
 
+/* Pacify compiler: -Werror=unused-but-set-variable  */
+#if 0
   len = wcstombs(NULL, ws, 0);
+#else
+  wcstombs(NULL, ws, 0);
+#endif
 
   return 0;
 }
