0e1148b40f16b654ad9af40d6ce282b7b735e9a3
juan.guerrero <juan.guerrero>
2013-06-29 11:28:30 +0000

    Rearrange the arguments in the correct sequence.

--- src/libc/ansi/stdlib/calloc.c
+++ src/libc/ansi/stdlib/calloc.c
@@ -1,12 +1,14 @@
+/* Copyright (C) 2013 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #include <stdlib.h>
 #include <string.h>
 
 void *
-calloc(size_t size, size_t nelem)
+calloc(size_t nelem, size_t elsize)
 {
-  void *rv = malloc(size*nelem);
+  register size_t size = 	nelem * elsize;
+  void *rv = malloc(size);
   if (rv)
-    memset(rv, 0, size*nelem);
+    memset(rv, 0, size);
   return rv;
 }
