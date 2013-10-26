9360cbdfcc56549347e9b96c62fa632dcf5a27b2
juan.guerrero <juan.guerrero>
2013-10-26 10:26:59 +0000

    Check driver.

---
+++ tests/cygnus/t-main.h
@@ -0,0 +1,30 @@
+/* Copyright (C) 2013 DJ Delorie, see COPYING.DJ for details */
+
+
+#ifndef T_MAIN_H
+# define T_MAIN_H
+
+# include <stdio.h>
+# include <math.h>
+# include <libc/ieee.h>
+
+
+int llrint_test(void);
+int llrintl_test(void);
+int llround_test(void);
+int llroundf_test(void);
+int llroundl_test(void);
+int lrint_test(void);
+int lrintf_test(void);
+int lrintl_test(void);
+int lround_test(void);
+int lroundf_test(void);
+int lroundl_test(void);
+int round_test(void);
+int roundf_test(void);
+int roundl_test(void);
+int trunc_test(void);
+int truncf_test(void);
+int truncl_test(void);
+
+#endif  /* T_MAIN_H */
