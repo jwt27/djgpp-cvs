398667d00c57b5924b44a9588dc516b2110d869c
juan.guerrero <juan.guerrero>
2013-11-16 21:48:31 +0000

    Implementation of finitel.

---
+++ src/libm/math/s_finitel.c
@@ -0,0 +1,25 @@
+/* Copyright (C) 2013 DJ Delorie, see COPYING.DJ for details */
+/*
+ * finitel(x) returns 1 is x is finite, else 0;
+ * no branching!
+ */
+
+#include <math.h>
+#include <libc/ieee.h>
+
+
+#ifdef __STDC__
+int
+finitel(long double x)
+#else
+int
+finite(x)
+long double x;
+#endif
+{
+  _longdouble_union_t ieee754;
+
+
+  ieee754.ld = x;
+  return (int)((unsigned int)(ieee754.ldt.exponent - 0x7FFF) >> 31);
+}
