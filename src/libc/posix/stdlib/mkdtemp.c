cdf07d63e34c2f49ed2a1d6836439af4b62a5519
juan.guerrero <juan.guerrero>
2014-01-10 22:40:55 +0000

    Implementation of mkdtemp.

---
+++ src/libc/posix/stdlib/mkdtemp.c
@@ -0,0 +1,33 @@
+/* Copyright (C) 2014 DJ Delorie, see COPYING.DJ for details */
+/* Copyright (C) 2013 DJ Delorie, see COPYING.DJ for details */
+#include <libc/symlink.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+char *
+mkdtemp(char *_template)
+{
+  char tmp_name[FILENAME_MAX];
+  char real_path[FILENAME_MAX];
+  int  rv;
+
+
+  do {
+    strcpy(tmp_name, _template);
+    errno = 0;
+  } while (mktemp(tmp_name) != NULL
+           && __solve_symlinks(tmp_name, real_path)
+           && (rv = mkdir(real_path, S_IWUSR))
+           && errno == EEXIST);
+
+  if (rv == 0)
+  {
+    strcpy(_template, tmp_name);
+    return _template;
+  }
+  else
+    return NULL;
+}
