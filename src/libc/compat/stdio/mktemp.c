e06c7bb28eea881caa1e0bc8a5c4f76c9b3a4e31
juan.guerrero <juan.guerrero>
2014-01-10 20:16:49 +0000

    Added POSIX compliant behavior to mktemp if LFN support is available.

--- src/libc/compat/stdio/mktemp.c
+++ src/libc/compat/stdio/mktemp.c
@@ -1,3 +1,4 @@
+/* Copyright (C) 2014 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 2013 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 2009 DJ Delorie, see COPYING.DJ for details */
 /* Copyright (C) 2000 DJ Delorie, see COPYING.DJ for details */
@@ -6,75 +7,91 @@
 #include <libc/stubs.h>
 #include <libc/bss.h>
 #include <libc/symlink.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 static int mktemp_count = -1;
 
 char *
 mktemp(char *_template)
 {
-  static int count = 0;
-  char *cp, *dp;
-  int i, len, xcount, loopcnt;
-  char real_path[FILENAME_MAX];
+  register int i, len, xcount;
+  unsigned int use_lfn = _USE_LFN;
 
-  /* Reinitialize counter if we were restarted (emacs).  */
-  if (__bss_count != mktemp_count)
+  for (i = 0; _template[i]; i++)
+    ;
+  for (xcount = 0, len = i; xcount < 6  && _template[--i] == 'X'; xcount++)
+    ;
+  if (use_lfn && xcount < 6)
   {
-    mktemp_count = __bss_count;
-    count = 0;
+    errno = EINVAL;
+    return NULL;
   }
-
-  len = strlen(_template);
-  cp = _template + len;
-
-  xcount = 0;
-  while (xcount < 6 && cp > _template && cp[-1] == 'X')
-    xcount++, cp--;
-
-  if (xcount)
+  else
   {
-    dp = cp;
-    while (dp > _template && dp[-1] != '/' && dp[-1] != '\\' && dp[-1] != ':')
-      dp--;
+    static int count = 0;
 
-    /* Keep the first characters of the template, but turn the rest into
-       Xs.  */
-    while (cp > dp + 8 - xcount)
+    /* Reinitialize counter if we were restarted (emacs).  */
+    if (__bss_count != mktemp_count)
     {
-      *--cp = 'X';
-      xcount = (xcount >= 6) ? 6 : 1 + xcount;
+      mktemp_count = __bss_count;
+      count = 0;
     }
 
-    /* If dots occur too early -- squash them.  */
-    while (dp < cp)
+    if (xcount)
     {
-      if (*dp == '.') *dp = 'a';
-      dp++;
-    }
+      char *cp, *dp;
+      char real_path[FILENAME_MAX];
+      int loopcnt;
 
-    /* Try to add ".tmp" to the filename.  Truncate unused Xs.  */
-    if (cp + xcount + 3 < _template + len)
-      strcpy (cp + xcount, ".tmp");
-    else
-      cp[xcount] = 0;
+      dp = cp = _template + i;
 
-    /* This loop can run up to 2<<(5*6) times, or about 10^9 times.  */
-    for (loopcnt = 0; loopcnt < (1 << (5 * xcount)); loopcnt++)
-    {
-      int c = count++;
-      for (i = 0; i < xcount; i++, c >>= 5)
-        cp[i] = "abcdefghijklmnopqrstuvwxyz012345"[c & 0x1f];
-      if (!__solve_symlinks(_template, real_path))
+      while (dp > _template && dp[-1] != '/' && dp[-1] != '\\' && dp[-1] != ':')
+        dp--;
+
+      if (!use_lfn)
       {
-        *_template = 0;
-        return NULL;
+        /* Keep the first characters of the template,
+           but turn the rest into Xs.  */
+        while (cp > dp + 8 - xcount)
+        {
+          *--cp = 'X';
+          xcount = (xcount >= 6) ? 6 : 1 + xcount;
+        }
+
+        /* If dots occur too early -- squash them.  */
+        while (dp < cp)
+        {
+          if (*dp == '.') *dp = 'a';
+          dp++;
+        }
+
+        /* Try to add ".tmp" to the filename.  Truncate unused Xs.  */
+        if (cp + xcount + 3 < _template + len)
+          strcpy (cp + xcount, ".tmp");
+        else
+          cp[xcount] = 0;
+
       }
-      if (!__file_exists(real_path))
-        return _template;
+
+      /* This loop can run up to 2<<(5*6) times, or about 10^9 times.  */
+      for (loopcnt = 0; loopcnt < (1 << (5 * xcount)); loopcnt++)
+      {
+        int c = count++;
+        for (i = 0; i < xcount; i++, c >>= 5)
+          cp[i] = "abcdefghijklmnopqrstuvwxyz012345"[c & 0x1f];
+        if (!__solve_symlinks(_template, real_path))
+        {
+          *_template = 0;
+          return NULL;
+        }
+        if (!__file_exists(real_path))
+          return _template;
+      }
     }
   }
 
@@ -92,42 +109,50 @@ main(void)
   char *s, *s0;
 
   s = strdup (s0 = "/usr/foo/dddddddXXXXXX");
+  errno = 0;
   mktemp (s);
-  printf ("%s -> %s\n", s0, s);
+  printf ("%s -> %s  errno = %d\n", s0, s, errno);
 
   s = strdup (s0 = "/usr/foo/ddddddXXXXXX");
+  errno = 0;
   mktemp (s);
-  printf ("%s -> %s\n", s0, s);
+  printf ("%s -> %s  errno = %d\n", s0, s, errno);
 
   s = strdup (s0 = "/usr/foo/dddddXXXXXX");
+  errno = 0;
   mktemp (s);
-  printf ("%s -> %s\n", s0, s);
+  printf ("%s -> %s  errno = %d\n", s0, s, errno);
 
   s = strdup (s0 = "/usr/foo/ddddXXXXXX");
+  errno = 0;
   mktemp (s);
-  printf ("%s -> %s\n", s0, s);
+  printf ("%s -> %s  errno = %d\n", s0, s, errno);
 
   s = strdup (s0 = "/usr/foo/dddXXXXXX");
+  errno = 0;
   mktemp (s);
-  printf ("%s -> %s\n", s0, s);
+  printf ("%s -> %s  errno = %d\n", s0, s, errno);
 
   s = strdup (s0 = "/usr/foo/.dddXXXXXX");
+  errno = 0;
   mktemp (s);
-  printf ("%s -> %s\n", s0, s);
+  printf ("%s -> %s  errno = %d\n", s0, s, errno);
 
   s = strdup (s0 = "/usr/foo/dddXXYXXX");
+  errno = 0;
   mktemp (s);
-  printf ("%s -> %s\n", s0, s);
+  printf ("%s -> %s  errno = %d\n", s0, s, errno);
 
   s = strdup (s0 = "/usr/foo/XXXXXX");
+  errno = 0;
   mktemp (s);
-  printf ("%s -> %s\n", s0, s);
+  printf ("%s -> %s  errno = %d\n", s0, s, errno);
 
   s = strdup (s0 = "/usr/foo/XXXXXXXX");
+  errno = 0;
   mktemp (s);
-  printf ("%s -> %s\n", s0, s);
+  printf ("%s -> %s  errno = %d\n", s0, s, errno);
 
   return 0;
 }
 #endif
-
