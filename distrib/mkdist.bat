63a353129916897fe9b3a27d0cda11970f28181b
andris <andris>
2015-05-04 03:30:09 +0000

    distrib/mkdist.bat: update for version 2.05

--- distrib/mkdist.bat
+++ distrib/mkdist.bat
@@ -3,10 +3,10 @@
 cp ../readme.1st ../zips
 cp ../copying* ../zips
 
-call mkdist1 djdev204 Development Kit and Runtime
-call mkdist1 djlsr204 Base Library Sources
-call mkdist1 djtst204 Test Programs
-call mkdist1 djcrx204 For Cross-to-DOS Compiling (subset of djdev)
-call mkdist1 djtzn204 Extra Timezone Support Files
-call mkdist1 djtzs204 Timezone Sources and Utils
+call mkdist1 djdev205 Development Kit and Runtime
+call mkdist1 djlsr205 Base Library Sources
+call mkdist1 djtst205 Test Programs
+call mkdist1 djcrx205 For Cross-to-DOS Compiling (subset of djdev)
+call mkdist1 djtzn205 Extra Timezone Support Files
+call mkdist1 djtzs205 Timezone Sources and Utils
 
