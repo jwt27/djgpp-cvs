34761532acbed23627f331dcdd1d2a1cc84a507b
J.W. Jagersma <jwjagersma@gmail.com>
2019-07-13 06:13:21 +0200

    fix multiple definition errors in dbgcom.c

--- src/debug/common/dbgcom.c
+++ src/debug/common/dbgcom.c
@@ -1338,13 +1338,13 @@ int invalid_sel_addr(short sel, unsigned a, unsigned len, char for_write)
     ("										\n\
       movw  %2,%%ax								\n\
       verr  %%ax								\n\
-      jnz   .Ldoes_not_has_read_right						\n\
+      jnz   .Ldoes_not_has_read_right%=					\n\
       movb  $1,%0								\n\
-.Ldoes_not_has_read_right:							\n\
+.Ldoes_not_has_read_right%=:							\n\
       verw  %%ax								\n\
-      jnz   .Ldoes_not_has_write_right						\n\
+      jnz   .Ldoes_not_has_write_right%=					\n\
       movb  $1,%1								\n\
-.Ldoes_not_has_write_right: "
+.Ldoes_not_has_write_right%=: "
      : "=qm" (read_allowed), "=qm" (write_allowed)
      : "g" (sel)
      );
