5d9ab31cb6ec2012840d3b428f0af8d16a8ac707
Stas Sergeev <stsp@users.sourceforge.net>
2022-10-27 16:20:34 +0500

    disable ___stdio_cleanup_proc hack

--- include/libc/stdiohk.h
+++ include/libc/stdiohk.h
@@ -23,7 +23,9 @@ extern "C" {
    code for the stdio flush/fclose stuff. That .o causes the
    hook function to get initialized also. */
 
+#ifndef __x86_64__
 __asm__(".long ___stdio_cleanup_proc");
+#endif
 
 #endif /* !_POSIX_SOURCE */
 #endif /* !__STRICT_ANSI__ */
