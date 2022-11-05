045c9b64dcf0f6bb28a7127534075ed6fb9c033a
Stas Sergeev <stsp@users.sourceforge.net>
2022-11-05 23:56:56 +0500

    parsers: add mkproto.sh
    This convoluted script is supposed to mediate between djgpp's
    build process and fdpp's thunk_gen.
    Its quite amazing how many logic can be squeezed into such a small
    script.

---
+++ parsers/mkproto.sh
@@ -0,0 +1,29 @@
+extr_proto() {
+    readtags -t "$1" "$2" | \
+	expand | \
+	sed -E \
+	    -e 's/.*\/\^(.*;).*/\1/' \
+	    -e 's/__P\((.*)\);/\1;/' \
+	    -e "s/(.*)( [^ (]+)( *\([^(]+.*;)/\1 $3 \2\3/" \
+	       | \
+	tr -s '[:blank:]'
+}
+
+list_syms() {
+    nm -A "$1" | grep " $2 " | tr -s '[:blank:]' | cut -d " " -f 3 | \
+	sed 's/^_//'
+}
+
+TF=/tmp/tagsxx
+TL=/tmp/a.so
+set -e
+ld -melf_i386 -shared -Bsymbolic -o $TL --whole-archive "$1" 2>/dev/null
+find $2 -name libm -prune -o -print | \
+	ctags -L - --kinds-C=p --pattern-length-limit=0 -f $TF
+# https://stackoverflow.com/questions/11003418/calling-shell-functions-with-xargs
+export -f extr_proto
+list_syms $TL T | xargs -I '{}' bash -c "extr_proto $TF '{}' ASMFUNC" | nl -n ln -v 0 >$3
+list_syms $TL U | xargs -I '{}' bash -c "extr_proto $TF '{}' ASMCFUNC" | nl -n ln -v 0 >$4
+
+rm $TF
+rm $TL
