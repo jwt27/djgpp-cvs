f723af1e2543f6d6c7c164b3dcbdd8dbf1dbafc3
Stas Sergeev <stsp@users.sourceforge.net>
2022-11-25 19:47:26 +0500

    more work on thunks

--- parsers/gen_asms.sh
+++ parsers/gen_asms.sh
@@ -7,9 +7,16 @@ gen_asms() {
     grep -R " ASM(" . "$1" | grep EXTERN | sed -E 's/.+\((.+)\);/asmsym _\1/'
 }
 
+gen_asyms() {
+    grep -R " ASM(" . "$1" | grep EXTERN | \
+	sed -E 's/.+\((.+)\);/\(void \*\*\)\&__\1,/'
+}
+
 case "$1" in
     0) gen_incs "$2"
     ;;
     1) gen_asms "$2"
+    ;;
+    2) gen_asyms "$2"
     ;;
 esac
