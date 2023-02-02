fd554eaa810f1ab20ae15943278722def192848c
Stas Sergeev <stsp@users.sourceforge.net>
2023-02-02 18:01:28 +0500

    parsers: more work on ASM

--- parsers/gen_asms.sh
+++ parsers/gen_asms.sh
@@ -1,6 +1,6 @@
 gen_incs() {
-    cd "$1" && egrep -R " ASM\(| ASM_P\(" . | grep EXTERN | cut -d ":" -f 1 | uniq | \
-	sed -E 's/\.\/(.+)/#include "\1"/'
+    cd "$1" && egrep -R " ASM\(| ASM_P\(| ASM_N\(" . | grep EXTERN | \
+	cut -d ":" -f 1 | uniq | sed -E 's/\.\/(.+)/#include "\1"/'
 }
 
 gen_incsn() {
@@ -9,13 +9,15 @@ gen_incsn() {
 }
 
 gen_asms() {
-    grep -R " ASM(" . "$1" | grep EXTERN | \
+    egrep -R " ASM\(| ASM_P\(| ASM_N\(" . "$1" | grep EXTERN | \
 	sed -E 's/.+\((.+)\);.*/asmsym _\1/' | sort | uniq
 }
 
 gen_asyms() {
-    grep -R " ASM(" . "$1" | grep EXTERN | \
+    egrep -R " ASM\(| ASM_N\(" . "$1" | grep EXTERN | \
 	sed -E 's/.+\((.+)\);.*/\(void \*\*\)\&__\1,/' | sort | uniq
+    grep -R " ASM_P(" . "$1" | grep EXTERN | \
+	sed -E 's/.+\(.+, (.+)\);.*/\(void \*\*\)\&__\1,/' | sort | uniq
 }
 
 case "$1" in
