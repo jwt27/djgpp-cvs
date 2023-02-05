25365f7b1e75124d1b905b27bb9949780301781c
Stas Sergeev <stsp@users.sourceforge.net>
2023-02-05 23:09:32 +0500

    extend stubinfo

--- include/libc/stubinfo.h
+++ include/libc/stubinfo.h
@@ -16,7 +16,11 @@
 #define STUBINFO_BASENAME 0x2c
 #define STUBINFO_ARGV0 0x34
 #define STUBINFO_DPMI_SERVER 0x44
-#define STUBINFO_END 0x54
+#define STUBINFO_STUBINFO_VER 0x54
+#define STUBINFO_SELF_FD 0x58
+#define STUBINFO_PAYLOAD_OFFS 0x5C
+#define STUBINFO_PAYLOAD_SIZE 0x60
+#define STUBINFO_END 0x64
 #ifndef __ASSEMBLER__
 typedef struct {
   char magic[16];
@@ -33,6 +37,11 @@ typedef struct {
   char basename[8];
   char argv0[16];
   char dpmi_server[16];
+  /* standard djgpp stubinfo ends here */
+  unsigned long stubinfo_ver;
+  long self_fd;
+  unsigned long payload_offs;
+  long payload_size;
 } _GO32_StubInfo;
 #endif
 
