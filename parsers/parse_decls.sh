195cb096cc2255216f43c09e5c4959404e1f8821
Stas Sergeev <stsp@users.sourceforge.net>
2023-06-06 22:15:03 +0500

    port in parse_decls.sh from fdpp

---
+++ parsers/parse_decls.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+gen_plt_asmc() {
+	grep ASMFUNC $1 | \
+		sed -E 's/([0-9]+)[ \t]+([^ \t\(]+[ \t]+)+([^ \(]+) *\(.+/asmcsym \3, \1/'
+}
+
+gen_plt_asmc $2
