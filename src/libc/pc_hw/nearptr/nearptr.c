a2910f5c0b2d0b1750de8db7622130d604392221
Stas Sergeev <stsp@users.sourceforge.net>
2024-02-19 14:46:06 +0500

    nearptr: disable limit check
    This speeds up the things a bit.

--- src/libc/pc_hw/nearptr/nearptr.c
+++ src/libc/pc_hw/nearptr/nearptr.c
@@ -13,8 +13,8 @@
 int __djgpp_nearptr_enable(void)
 {
   if(!__dpmi_set_segment_limit(_my_ds(), 0xffffffffU)) {
-    if(__dpmi_get_segment_limit(_my_ds()) != 0xffffffffU)
-      return 0;		/* We set it but DPMI ignored/truncated it */
+//    if(__dpmi_get_segment_limit(_my_ds()) != 0xffffffffU)
+//      return 0;		/* We set it but DPMI ignored/truncated it */
     __dpmi_set_segment_limit(__djgpp_ds_alias, 0xffffffffU);
     __dpmi_set_segment_limit(_my_cs(), 0xffffffffU);
     _crt0_startup_flags |= _CRT0_FLAG_NEARPTR;
