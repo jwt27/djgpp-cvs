741e5644e9e1692842d80c0ee539b37c2e491eda
Stas Sergeev <stsp@users.sourceforge.net>
2024-02-21 22:04:07 +0500

    fix ASM_N

--- include/dj64/asm_inc.h
+++ include/dj64/asm_inc.h
@@ -4,7 +4,7 @@
 #define __ASM_PTR(t, v) EXTERN ASM_P(t, v)
 #define __ASM_ARR(t, v) EXTERN t ASM_ARR(v)
 #define __ASM_FUNC(v) EXTERN ASM_F(v)
-#define __ASM_N(t, v) extern ASM(t, v)
+#define __ASM_N(t, v) EXTERN t ASM_N(v)
 #define SEMIC ;
 #include "glob_asm.h"
 #undef __ASM
