12bd10ebabdc5aabda541e69ffb33f9858dd1d66
Stas Sergeev <stsp@users.sourceforge.net>
2024-03-26 15:01:51 +0300

    add DJ64_PRINT_SCREEN

--- include/djdev64/dj64init.h
+++ include/djdev64/dj64init.h
@@ -24,8 +24,8 @@
 
 typedef int (dj64cdispatch_t)(int handle, int libid, int fn, unsigned esi,
         uint8_t *sp);
-#define DJ64_API_VER 3
-enum { DJ64_PRINT_LOG, DJ64_PRINT_TERMINAL };
+#define DJ64_API_VER 4
+enum { DJ64_PRINT_LOG, DJ64_PRINT_TERMINAL, DJ64_PRINT_SCREEN };
 
 /* pushal */
 typedef struct {
