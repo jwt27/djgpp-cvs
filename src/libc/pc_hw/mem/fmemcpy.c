4ee4f5ec1ad4eae8da15f155fb9b97cc7522f5de
Stas Sergeev <stsp@users.sourceforge.net>
2024-06-29 12:09:43 +0300

    fmemcpy.c: re-license to LGPLv3+ [#5]

--- src/libc/pc_hw/mem/fmemcpy.c
+++ src/libc/pc_hw/mem/fmemcpy.c
@@ -1,19 +1,33 @@
 /*
- *  Copyright (C) 2023  stsp
+ *  dj64 - 64bit djgpp-compatible tool-chain
+ *  Copyright (C) 2024  @stsp
+ *  Copyright (C) 2023  stsp, comcom32 project
  *
  *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
+ *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/*
+ * NOTE: this file is a port of fmemcpy.c file from comcom32 project.
+ * In comcom32 project it is distributed under the terms of GNU GPLv3+
+ * and is copyrighted (C) 2023  stsp.
+ * As a sole author of the aforementioned fmemcpy.c, I donate the
+ * code to dj64dev project, allowing to re-license it under the terms
+ * of GNU LGPLv3+.
+ *
+ * --stsp
+ */
+
 #include <sys/nearptr.h>
 #include <dpmi.h>
 #include <string.h>
