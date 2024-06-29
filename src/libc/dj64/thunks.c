ca93ea5c6d9e760f58fe98ce3ff1ed0e039c1843
Stas Sergeev <stsp@users.sourceforge.net>
2024-06-29 12:13:04 +0300

    thunks.c: re-license to LGPLv3+ [#5]

--- src/libc/dj64/thunks.c
+++ src/libc/dj64/thunks.c
@@ -2,18 +2,32 @@
  *  dj64 - 64bit djgpp-compatible tool-chain
  *  Copyright (C) 2021-2024  @stsp
  *
+ *  FDPP - freedos port to modern C++
+ *  Copyright (C) 2018  Stas Sergeev (stsp)
+ *
  *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
+ *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * NOTE: this file is a port of thunks.cc file from fdpp project.
+ * In fdpp project it is distributed under the terms of GNU GPLv3+
+ * and is copyrighted (C) 2018-2023  Stas Sergeev (stsp).
+ * As a sole author of the aforementioned dosobj.cc, I donate the
+ * code to dj64dev project, allowing to re-license it under the terms
+ * of GNU LGPLv3+.
+ *
+ * --stsp
  */
 
 #include <setjmp.h>
