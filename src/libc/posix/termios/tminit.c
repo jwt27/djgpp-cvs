cf356e5f4d081d21a358a7de82ab8615b83efe4a
Stas Sergeev <stsp@users.sourceforge.net>
2024-07-14 14:18:39 +0300

    tminit: define swappable context

--- src/libc/posix/termios/tminit.c
+++ src/libc/posix/termios/tminit.c
@@ -22,6 +22,37 @@ struct tty *__libc_tty_p = &__libc_tty_internal;
 /* global only in the termios functions */
 int __libc_termios_hook_common_count = -1;
 
+struct tm_state {
+  struct tm_state *prev;
+  struct tty __libc_tty_internal;
+  struct tty *__libc_tty_p;
+  int __libc_termios_hook_common_count;
+};
+
+static struct tm_state *tms;
+
+static void tms_init(void)
+{
+  __libc_tty_internal = (struct tty)TTYDEFAULT;
+  __libc_tty_p = &__libc_tty_internal;
+  __libc_termios_hook_common_count = -1;
+}
+static void tms_pre(void)
+{
+#define _SV(x) tms->x = x
+  _SV(__libc_tty_internal);
+  _SV(__libc_tty_p);
+  _SV(__libc_termios_hook_common_count);
+}
+static void tms_post(void)
+{
+#define _RS(x) x = tms->x
+  _RS(__libc_tty_internal);
+  _RS(__libc_tty_p);
+  _RS(__libc_termios_hook_common_count);
+}
+DJ64_DEFINE_SWAPPABLE_CONTEXT2(tm_state, tms,
+    tms_init(), tms_pre(), tms_post());
 
 /* static functions */
 static void __libc_termios_fflushall(void);
