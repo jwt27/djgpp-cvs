0947b89a17c2f635d2a53db3c00b9f59325614ea
Stas Sergeev <stsp@users.sourceforge.net>
2024-07-14 14:28:13 +0300

    termios: define more swappable contexts

--- src/libc/posix/termios/readhook.c
+++ src/libc/posix/termios/readhook.c
@@ -4,3 +4,28 @@
 
 ssize_t (*__libc_read_termios_hook)(int handle, void *buffer, size_t count,
 				    ssize_t *rv) = NULL;
+
+struct tmr_state {
+  struct tmr_state *prev;
+  ssize_t (*__libc_read_termios_hook)(int handle, void *buffer, size_t count,
+				      ssize_t *rv);
+};
+
+static struct tmr_state *sts;
+
+static void tmr_init(void)
+{
+  __libc_read_termios_hook = NULL;
+}
+static void tmr_pre(void)
+{
+#define _SV(x) sts->x = x
+  _SV(__libc_read_termios_hook);
+}
+static void tmr_post(void)
+{
+#define _RS(x) x = sts->x
+  _RS(__libc_read_termios_hook);
+}
+DJ64_DEFINE_SWAPPABLE_CONTEXT2(tmr_state, sts,
+    tmr_init(), tmr_pre(), tmr_post());
--- src/libc/posix/termios/writhook.c
+++ src/libc/posix/termios/writhook.c
@@ -4,3 +4,28 @@
 
 ssize_t (*__libc_write_termios_hook)(int handle, const void *buffer,
 				     size_t count, ssize_t *rv) = NULL;
+
+struct tmw_state {
+  struct tmw_state *prev;
+  ssize_t (*__libc_write_termios_hook)(int handle, const void *buffer,
+				       size_t count, ssize_t *rv);
+};
+
+static struct tmw_state *sts;
+
+static void tmw_init(void)
+{
+  __libc_write_termios_hook = NULL;
+}
+static void tmw_pre(void)
+{
+#define _SV(x) sts->x = x
+  _SV(__libc_write_termios_hook);
+}
+static void tmw_post(void)
+{
+#define _RS(x) x = sts->x
+  _RS(__libc_write_termios_hook);
+}
+DJ64_DEFINE_SWAPPABLE_CONTEXT2(tmw_state, sts,
+    tmw_init(), tmw_pre(), tmw_post());
