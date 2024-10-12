2f395fd7184590362adc2c4ceee0cf5d88f97a1e
Stas Sergeev <stsp@users.sourceforge.net>
2024-10-12 23:11:17 +0300

    export __dj_errno

--- include/dj64/a_mac.h
+++ include/dj64/a_mac.h
@@ -2,3 +2,4 @@
 #define _stubinfo __ASYM(_stubinfo)
 #define _go32_info_block __ASYM_L(_go32_info_block)
 #define __djgpp_base_address __ASYM_L(__djgpp_base_address)
+#define __dj_errno __ASYM_L(__dj_errno)
