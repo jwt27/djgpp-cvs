493735b543c5d434612c2c7c35d169a261222739
Stas Sergeev <stsp@users.sourceforge.net>
2024-10-18 00:40:34 +0300

    dj64.mk: different solution for load address problem
    Suggested by Nick Clifton, see
    https://sourceware.org/bugzilla/show_bug.cgi?id=32271

--- dj64.mk
+++ dj64.mk
@@ -69,10 +69,7 @@ ifeq ($(DJ64STATIC),1)
 XLDFLAGS += $(shell pkg-config --static --libs dj64static)
 DJ64_XLDFLAGS += -f 0x4000
 else
-XLDFLAGS += $(shell pkg-config --variable=crt0 dj64) \
-  --section-start=.note.gnu.property=0x08148000 \
-  --section-start=.note.package=0x08149000 \
-  --section-start=.text=0x0814a000
+XLDFLAGS += $(shell pkg-config --variable=crt0 dj64) -Ttext-segment=0x08148000
 endif
 $(XELF): $(AS_OBJECTS) $(PLT_O)
 	$(XLD) $^ $(XLDFLAGS) -o $@
